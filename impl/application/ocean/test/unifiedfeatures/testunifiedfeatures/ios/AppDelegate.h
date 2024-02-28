629d8f36cb57e4a73d27152297439f1b80794462
Nils Plath <nilsplath@meta.com>
2024-02-28 14:42:09 -0800

    Move ocean/unifiedfeatures to the metaonly directory
    Summary:
    This library, its tests, and its applications are not meant to be open-source. We're moving into the meta-internal source directory.
    
    Also updating all locations that use these libraries.
    
    Differential Revision: D54288233
    
    fbshipit-source-id: 35e5df0d3c468ea8838074cf2a17a2ce2fc5695e

--- impl/application/ocean/test/unifiedfeatures/testunifiedfeatures/ios/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-#import <UIKit/UIKit.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@end
--- impl/application/ocean/test/unifiedfeatures/testunifiedfeatures/ios/ViewController.h
+++
@@ -1,9 +0,0 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-#import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
-
-@property (weak, nonatomic) IBOutlet UITextView *controllerTextView;
-
-@end
