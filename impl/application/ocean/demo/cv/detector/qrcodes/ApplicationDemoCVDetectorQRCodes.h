f6ae67230d2c9db72a33169e6044aa8540937683
Nils Plath <nilsplath@meta.com>
2024-02-28 18:49:41 -0800

    Move application header for the QR code demos to xplat/ocean
    Summary: Moves the application header, adds a corresponding `BUCK` target files for it, and updates all locations that include this header file
    
    Differential Revision: D54338815
    
    fbshipit-source-id: bc72be184ae7c1f52528c45469ed1f1d2e3b1c75

---
+++ impl/application/ocean/demo/cv/detector/qrcodes/ApplicationDemoCVDetectorQRCodes.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef APPLICATION_DEMO_CV_DETECTOR_QRCODES_H
+#define APPLICATION_DEMO_CV_DETECTOR_QRCODES_H
+
+#include "application/ocean/demo/cv/detector/ApplicationDemoCVDetector.h"
+
+/**
+ * @ingroup applicationdemocvdetector
+ * @defgroup applicationdemocvdetectorqrcodes Demos applications of the QR Code library
+ * @{
+ * All demo applications demonstrates functionalities of the QR code library inside of the Ocean Computer Vision Detector libraries.<br>
+ * @}
+ */
+
+#endif // APPLICATION_DEMO_CV_DETECTOR_QRCODES_H
