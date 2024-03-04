943047b02c0b9f5622a762bbc5873cea8158d723
Alexander Schneider <aschneider1@meta.com>
2024-03-04 10:34:32 -0800

    move ocean/application/demo/misc/edenworkaround to xplat/ocean
    Reviewed By: enpe
    
    Differential Revision: D54438340
    
    fbshipit-source-id: 329d7d1a4fb6c49012631eb767236bf06a9cbf3c

---
+++ impl/application/ocean/demo/misc/ApplicationDemoMisc.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef APPLICATION_DEMO_MISC_H
+#define APPLICATION_DEMO_MISC_H
+
+#include "application/ocean/demo/ApplicationDemo.h"
+
+/**
+ * @ingroup applicationdemo
+ * @defgroup applicationdemomisc Miscellaneous applications
+ * @{
+ * Several miscellaneous demo applications.<br>
+ * @}
+ */
+
+#endif // APPLICATION_DEMO_MISC_H
