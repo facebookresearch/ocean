add582ec8fbf5adedf5ba0cf01a57a6c191b7dcc
Alexander Schneider <aschneider1@meta.com>
2024-03-04 10:26:54 -0800

    move demo/media/mediafoundation to xplat/ocean
    Reviewed By: enpe
    
    Differential Revision: D54435855
    
    fbshipit-source-id: f0712a43e911ba82035e55bec8a5205888239e5e

---
+++ impl/application/ocean/demo/media/mediafoundation/win/MediaFoundation.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_MEDIA_MEDIA_FOUNDATION_WIN_MEDIA_FOUNDATION_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_MEDIA_MEDIA_FOUNDATION_WIN_MEDIA_FOUNDATION_H
+
+#include "application/ocean/demo/media/ApplicationDemoMedia.h"
+
+/**
+ * @ingroup applicationdemomedia
+ * @defgroup applicationdemomediamediafoundationwin Media Foundation (Windows)
+ * @{
+ * The demo application demonstrates the implementation of a simple Windows Media Foundation application.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_MEDIA_MEDIA_FOUNDATION_WIN_MEDIA_FOUNDATION_H
