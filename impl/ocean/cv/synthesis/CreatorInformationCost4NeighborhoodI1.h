f20246fb6a6c28185d2c7c423da0a809bbe2dc71
Nils Plath <nilsplath@meta.com>
2024-03-05 14:18:04 -0800

    Minor improvement
    Summary: Just some minor compiler appeasements that were required for our Xcode projects.
    
    Differential Revision:
    D54518926
    
    Privacy Context Container: L1191897
    
    fbshipit-source-id: 155ba315667298b79e5e73c033aa59d2bb172dc8

--- impl/ocean/cv/synthesis/CreatorInformationCost4NeighborhoodI1.h
+++ impl/ocean/cv/synthesis/CreatorInformationCost4NeighborhoodI1.h
@@ -166,8 +166,8 @@ template <unsigned int tWeightFactor, unsigned int tBorderFactor>
 template <unsigned int tChannels>
 void CreatorInformationCost4NeighborhoodI1<tWeightFactor, tBorderFactor>::createSubsetChannels(const unsigned int firstColumn, const unsigned int numberColumns, const unsigned int firstRow, const unsigned int numberRows) const
 {
-	const uint8_t* layerFrame = layerI1_.frame().constdata<uint8_t>();
-	const uint8_t* layerMask = layerI1_.mask().constdata<uint8_t>();
+	const uint8_t* layerFrame = layerI1_.frame().template constdata<uint8_t>();
+	const uint8_t* layerMask = layerI1_.mask().template constdata<uint8_t>();
 
 	const unsigned int layerFramePaddingElements = layerI1_.frame().paddingElements();
 	const unsigned int layerMaskPaddingElements = layerI1_.mask().paddingElements();
