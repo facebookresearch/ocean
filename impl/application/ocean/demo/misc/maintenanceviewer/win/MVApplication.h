3dcfbe1d25cbbbf6d5688c4308889696fa18751e
Alexander Schneider <aschneider1@meta.com>
2024-03-05 13:31:54 -0800

    move demo/misc/maintenanceviewer to xplat/ocean
    Reviewed By: enpe
    
    Differential Revision: D54547781
    
    fbshipit-source-id: 8f6518e622e92950753483c8d42dda9755045d50

---
+++ impl/application/ocean/demo/misc/maintenanceviewer/win/MVApplication.h
@@ -0,0 +1,19 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_MISC_MAINTENANCE_VIEWER_WIN_MV_APPLICATION_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_MISC_MAINTENANCE_VIEWER_WIN_MV_APPLICATION_H
+
+#include "application/ocean/demo/misc/ApplicationDemoMisc.h"
+
+/**
+ * @ingroup applicationdemomisc
+ * @defgroup applicationdemomiscmaintenanceviewerwin Maintenance Viewer (Windows)
+ * @{
+ * The demo application implements a basic viewer of maintenance data.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+using namespace Ocean;
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_MISC_MAINTENANCE_VIEWER_WIN_MV_APPLICATION_H
