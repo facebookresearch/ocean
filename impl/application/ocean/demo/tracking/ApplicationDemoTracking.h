04ea6ac2dbd2844c75fdb25b1975d5f1bca8e81c
Nils Plath <nilsplath@meta.com>
2024-03-06 13:43:47 -0800

    Add new Buck target for tracking applications
    Summary:
    * Moved the application header for the network applications
    * Added a new Buck target for this header file
    * Replaced the manual integration of the header file from all targets that require it (and added a dependency instead)
    
    Differential Revision: D54556450
    
    fbshipit-source-id: 8e162c6441747e0edaeddab0d97473675b148b99

---
+++ impl/application/ocean/demo/tracking/ApplicationDemoTracking.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef APPLICATION_DEMO_TRACKING_H
+#define APPLICATION_DEMO_TRACKING_H
+
+#include "application/ocean/demo/ApplicationDemo.h"
+
+/**
+ * @ingroup applicationdemo
+ * @defgroup applicationdemotracking Tracking demo applications
+ * @{
+ * All demo applications demonstrates some functionalities of the Ocean Tracking libraries.<br>
+ * @}
+ */
+
+#endif // APPLICATION_DEMO_TRACKING_H
