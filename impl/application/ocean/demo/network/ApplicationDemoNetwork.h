6b9213a13397a7d57485fb86ea2a6b57e604ddd6
Nils Plath <nilsplath@meta.com>
2024-03-05 19:38:01 -0800

    Add new target for the network applications
    Summary:
    * Moved the application header for the network applications
    * Added a new Buck target for this header file
    * Replaced the manual integration of the header file from all targets that require it (and added a dependency instead)
    
    Differential Revision: D54544971
    
    fbshipit-source-id: 09457252d0b69a83d6d2a06c9d97a39254471f2f

---
+++ impl/application/ocean/demo/network/ApplicationDemoNetwork.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef APPLICATION_DEMO_NETWORK_H
+#define APPLICATION_DEMO_NETWORK_H
+
+#include "application/ocean/demo/ApplicationDemo.h"
+
+/**
+ * @ingroup applicationdemo
+ * @defgroup applicationdemonetwork Network demo applications
+ * @{
+ * All demo applications demonstrates some functionalities of the Ocean Network libraries.<br>
+ * @}
+ */
+
+#endif // APPLICATION_DEMO_NETWORK_H
