79cb6e6bb5d743b47ceae11b3f6240b4d7bdf521
Nils Plath <nilsplath@meta.com>
2024-03-06 15:26:52 -0800

    Move demonetworkstreamingclient and demonetworkstreamingserver to xplat/ocean
    Summary: Moving these demos to the new code location.
    
    Differential Revision: D54546944
    
    fbshipit-source-id: 8eaf55749171a7bb6779bb2a5eda1c120e9c74ad

---
+++ impl/application/ocean/demo/network/streamingserver/StreamingServer.h
@@ -0,0 +1,19 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_NETWORK_STREAMINGSERVER_STREAMINGSERVER_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_NETWORK_STREAMINGSERVER_STREAMINGSERVER_H
+
+#include "application/ocean/demo/network/ApplicationDemoNetwork.h"
+
+/**
+ * @ingroup applicationdemonetwork
+ * @defgroup applicationdemonetworkstreamingserver Streaming Server
+ * @{
+ * The demo application demonstrates the usage of the streaming server.<br>
+ * Use this server in combination with the streaming client demo application.<br>
+ * This application is almost platform independent and is available on desktop platforms like e.g., Windows or OS X.
+ * @see applicationdemonetworkstreamingclient
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_NETWORK_STREAMINGSERVER_STREAMINGSERVER_H
