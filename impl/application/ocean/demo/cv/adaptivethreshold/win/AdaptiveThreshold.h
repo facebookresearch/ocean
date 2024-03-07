fb457ee809a97b0333825b96b458a8dd6c2d7562
Daniel Thornton <danthornton@meta.com>
2024-03-07 09:53:37 -0800

    Move demo/cv/adaptivethreshold to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/adaptivethreshold` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/adaptivethreshold` to
    `xplat/ocean/impl/application/ocean/demo/cv/adaptivethreshold`
    
    Reviewed By: enpe
    
    Differential Revision: D54610639
    
    fbshipit-source-id: 22cccff5c7e5bf5cf96186aaa805fe6f6deaa507

---
+++ impl/application/ocean/demo/cv/adaptivethreshold/win/AdaptiveThreshold.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_ADAPTIVE_THRESHOLD_WIN_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_ADAPTIVE_THRESHOLD_WIN_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvadaptivethresholdwin Adaptive Threshold (Windows)
+ * @{
+ * The demo application demonstrates the usage of an adaptive threshold to convert grayscale images to binary (black and white) images.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_ADAPTIVE_THRESHOLD_WIN_H
