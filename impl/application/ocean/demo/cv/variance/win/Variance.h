b3926c64d8ec7a50f2c6ed2f7ce918fb76730a90
Daniel Thornton <danthornton@meta.com>
2024-03-07 09:53:37 -0800

    Move demo/cv/variance to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/variance` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/variance` to
    `xplat/ocean/impl/application/ocean/demo/cv/variance`
    
    Reviewed By: enpe
    
    Differential Revision: D54610389
    
    fbshipit-source-id: 1770ccc46dcf9975901ad64ac220f3779f4a22b5

---
+++ impl/application/ocean/demo/cv/variance/win/Variance.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_VARIANCE_WIN_VARIANCE_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_VARIANCE_WIN_VARIANCE_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvvariancewin Frame variance (Windows)
+ * @{
+ * The demo application demonstrates how to determine the variance in a frame.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_VARIANCE_WIN_VARIANCE_H
