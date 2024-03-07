9316d5770764769562294a4d2dd5de29ac8c1c89
Daniel Thornton <danthornton@meta.com>
2024-03-07 09:53:37 -0800

    Move demo/cv/canvasfontcreator to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/canvasfontcreator` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/canvasfontcreator` to
    `xplat/ocean/impl/application/ocean/demo/cv/canvasfontcreator`
    
    Reviewed By: enpe
    
    Differential Revision: D54610352
    
    fbshipit-source-id: 22170cf8bd5215d57a35cffeb01447af58cfd8a4

---
+++ impl/application/ocean/demo/cv/canvasfontcreator/win/CanvasFontCreator.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_CANVASFONTCREATOR_WIN_CANVAS_FONT_CREATOR_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_CANVASFONTCREATOR_WIN_CANVAS_FONT_CREATOR_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvcanvasfontcreatorwin Canvas Font Creator (Windows)
+ * @{
+ * The demo application demonstrates how to create a new font for the CV::Canvas class.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_CANVASFONTCREATOR_WIN_CANVAS_FONT_CREATOR_H
