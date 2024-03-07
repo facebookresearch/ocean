ecabae828c7b55ba8244b80d1e64e143da7b21d0
Daniel Thornton <danthornton@meta.com>
2024-03-07 09:53:09 -0800

    Move shapedetector demo to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/detector/shapedetector` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/detector/shapedetector` to
    `xplat/ocean/impl/application/ocean/demo/cv/detector/shapedetector`
    
    Reviewed By: enpe
    
    Differential Revision: D54592696
    
    fbshipit-source-id: 1ce747f2b34e4e2a05d74a81397a2ece3508c62d

---
+++ impl/application/ocean/demo/cv/detector/shapedetector/win/ShapeDetectorMain.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_DETECTOR_SHAPEDETECTOR__WIN_SHAEP_DETECTOR_MAIN_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_DETECTOR_SHAPEDETECTOR__WIN_SHAEP_DETECTOR_MAIN_H
+
+#include "application/ocean/demo/cv/detector/ApplicationDemoCVDetector.h"
+
+/**
+ * @ingroup applicationdemocvdetector
+ * @defgroup applicationdemocvdetectorshapedetectorwin Shape Detector (Windows)
+ * @{
+ * The demo application demonstrates the usage of the shape detector.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_DETECTOR_SHAPEDETECTOR__WIN_SHAEP_DETECTOR_MAIN_H
