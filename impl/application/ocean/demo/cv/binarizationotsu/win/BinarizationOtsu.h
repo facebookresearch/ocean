207ebc68cd3eb712fa53b136e81f164f680fa7d7
Daniel Thornton <danthornton@meta.com>
2024-03-06 16:27:26 -0800

    Move demo/cv/framebinarizationotsu to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/framebinarizationotsu` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/framebinarizationotsu` to
    `xplat/ocean/impl/application/ocean/demo/cv/framebinarizationotsu`
    
    Reviewed By: enpe
    
    Differential Revision: D54603352
    
    fbshipit-source-id: d5339985cef90495a10bad09a51b439a3539f340

---
+++ impl/application/ocean/demo/cv/binarizationotsu/win/BinarizationOtsu.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_BINARIZATION_OTSU_WIN_BINARIZATION_OTSU_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_BINARIZATION_OTSU_WIN_BINARIZATION_OTSU_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvbinarizationotsuwin Binarization Otsu (Windows)
+ * @{
+ * The demo application demonstrates the usage of the Otsu algorithm to to convert grayscale images to binary (black and white) images.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_BINARIZATION_OTSU_WIN_BINARIZATION_OTSU_H
