afbc52c4f982275596108bd7fa9074ffebe8f682
Daniel Thornton <danthornton@meta.com>
2024-03-07 09:53:37 -0800

    Move demo/cv/imageanalyzer to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/imageanalyzer` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/imageanalyzer` to
    `xplat/ocean/impl/application/ocean/demo/cv/imageanalyzer`
    
    Differential Revision: D54604281
    
    fbshipit-source-id: 12b9f93392c406385e39d8b43c3d552757625ed2

---
+++ impl/application/ocean/demo/cv/imageanalyzer/win/ImageAnalyzerMain.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_IMAGEANALYZER_WIN_IMAGE_ANALYZER_MAIN_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_IMAGEANALYZER_WIN_IMAGE_ANALYZER_MAIN_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvimageanalyzerwin Image Analyzer (Windows)
+ * @{
+ * The demo application implements a simple analyzer for images e.g., showing graphs of pixel intensities.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_IMAGEANALYZER_WIN_IMAGE_ANALYZER_MAIN_H
