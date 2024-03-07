f30ccc8bce99d9d255701be996ae7e910dbe2141
Daniel Thornton <danthornton@meta.com>
2024-03-07 09:53:09 -0800

    Move orbmatching demo
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/detector/orbmatching` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/detector/orbmatching` to
    `xplat/ocean/impl/application/ocean/demo/cv/detector/orbmatching`
    
    Correct rendering of live feed
    
    Reviewed By: enpe
    
    Differential Revision: D54594662
    
    fbshipit-source-id: 40c0364a3fca3ac822f13e005ca05b274ac2fd16

---
+++ impl/application/ocean/demo/cv/detector/orbmatching/win/ORBMatching.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_DETECTOR_ORBMATCHING_WIN_ORBMATCHING_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_DETECTOR_ORBMATCHING_WIN_ORBMATCHING_H
+
+#include "application/ocean/demo/cv/detector/ApplicationDemoCVDetector.h"
+
+/**
+ * @ingroup applicationdemocvdetector
+ * @defgroup applicationdemocvdetectororbmatchingwin ORB Matching (Windows)
+ * @{
+ * The demo application demonstrates the matching of ORB descriptors.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_DETECTOR_ORBMATCHING_WIN_ORBMATCHING_H
