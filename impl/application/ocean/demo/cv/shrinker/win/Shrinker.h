8d0492a13c1385c6cb528d02dce61c66b5c1da2c
Daniel Thornton <danthornton@meta.com>
2024-03-07 09:53:37 -0800

    Move demo/cv/shrinker to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/shrinker` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/shrinker` to
    `xplat/ocean/impl/application/ocean/demo/cv/shrinker`
    
    Reviewed By: enpe
    
    Differential Revision: D54604883
    
    fbshipit-source-id: 2ce860326cc97496c65180d53d837f213365e4f9

---
+++ impl/application/ocean/demo/cv/shrinker/win/Shrinker.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_SHRINKER_WIN_SHRINKER_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_SHRINKER_WIN_SHRINKER_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvshrinkerwin Frame shrinker (Windows)
+ * @{
+ * The demo application demonstrates the usage of the frame shrinker.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_SHRINKER_WIN_SHRINKER_H
