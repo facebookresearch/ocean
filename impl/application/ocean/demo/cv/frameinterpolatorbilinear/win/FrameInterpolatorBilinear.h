8f22d85f7c0ff2da736efea8cca841c07fd7870b
Daniel Thornton <danthornton@meta.com>
2024-03-08 08:52:39 -0800

    Move demo/cv/frameinterpolator* to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/frameinterpolator*` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/frameinterpolator*` to
    `xplat/ocean/impl/application/ocean/demo/cv/frameinterpolator*`
    
    Differential Revision: D54650170
    
    fbshipit-source-id: c363e310d6ec3cba7f503e1e55518612544c80de

---
+++ impl/application/ocean/demo/cv/frameinterpolatorbilinear/win/FrameInterpolatorBilinear.h
@@ -0,0 +1,19 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_FRAME_INTERPOLATOR_BILINEAR_WIN_FRAME_INTERPOLATOR_BILINEAR_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_FRAME_INTERPOLATOR_BILINEAR_WIN_FRAME_INTERPOLATOR_BILINEAR_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvframeinterpolatorbilinearwin Frame Interpolator Bilinear (Windows)
+ * @{
+ * The demo application demonstrates the bilinear frame interpolator.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+using namespace Ocean;
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_FRAME_INTERPOLATOR_BILINEAR_WIN_FRAME_INTERPOLATOR_BILINEAR_H
---
+++ impl/application/ocean/demo/cv/frameinterpolatortrilinear/win/FITBitmapWindow.cpp
@@ -0,0 +1,75 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "application/ocean/demo/cv/frameinterpolatortrilinear/win/FITBitmapWindow.h"
+
+#include "ocean/base/HighPerformanceTimer.h"
+
+#include "ocean/platform/win/Utilities.h"
+
+FITBitmapWindow::FITBitmapWindow(HINSTANCE instance, const std::wstring& name, const Callback& callback) :
+	Window(instance, name),
+	callback_(callback)
+{
+	// nothing to do here
+}
+
+FITBitmapWindow::~FITBitmapWindow()
+{
+	// nothing to do here
+}
+
+void FITBitmapWindow::setFrame(const Frame& frame)
+{
+	nativeFrame_.copy(frame);
+	interpolateNativeFrame();
+}
+
+void FITBitmapWindow::modifyWindowClass(WNDCLASSW& windowClass)
+{
+	// suppress window background redrawing
+	windowClass.hbrBackground = CreateSolidBrush(RGB(0, 0, 0));
+}
+
+void FITBitmapWindow::onResize(const unsigned int clientWidth, const unsigned int clientHeight)
+{
+	Window::onResize(clientWidth, clientHeight);
+	interpolateNativeFrame();
+}
+
+void FITBitmapWindow::onPaint()
+{
+	if (windowBitmap_.isValid() == false || clientHeight() == 0 || windowBitmap_.height() == 0)
+	{
+		return;
+	}
+
+	BitBlt(dc(), 0, 0, clientWidth(), clientHeight(), windowBitmap_.dc(), 0, 0, SRCCOPY);
+}
+
+void FITBitmapWindow::interpolateNativeFrame()
+{
+	if (nativeFrame_.isNull() || nativeFrame_.pixelFormat() != FrameType::FORMAT_RGB24)
+	{
+		return;
+	}
+
+	HighPerformanceTimer timer;
+
+	const unsigned int targetWidth = clientWidth();
+	const unsigned int targetHeight = clientHeight();
+
+	if (targetWidth != 0u && targetHeight != 0u)
+	{
+		Frame targetFrame(FrameType(nativeFrame_, targetWidth, targetHeight));
+
+		if (callback_)
+		{
+			callback_(nativeFrame_, targetFrame);
+
+			const double time = timer.mseconds();
+
+			windowBitmap_.set(targetFrame);
+			Platform::Win::Utilities::textOutput(windowBitmap_.dc(), 5, 5, String::toAString(time) + std::string("ms"));
+		}
+	}
+}
