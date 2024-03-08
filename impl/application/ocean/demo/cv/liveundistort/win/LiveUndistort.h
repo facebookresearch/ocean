fb43306dc2161132b0d6d3c8356dc9b82c64834f
Daniel Thornton <danthornton@meta.com>
2024-03-08 11:45:12 -0800

    Move demo/cv/liveundistort to xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/liveundistort` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/liveundistort` to
    `xplat/ocean/impl/application/ocean/demo/cv/liveundistort`
    
    Reviewed By: enpe
    
    Differential Revision: D54651673
    
    fbshipit-source-id: b447bb904c7e1307b6341cc705c8109b6001bcb7

---
+++ impl/application/ocean/demo/cv/liveundistort/win/LiveUndistort.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_LIVE_UNDISTORT_WIN_MAIN_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_LIVE_UNDISTORT_WIN_MAIN_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvliveundistortwin Live Undistort (Windows)
+ * @{
+ * The demo application demonstrates the undistortion of live frame media objects.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_LIVE_UNDISTORT_WIN_MAIN_H
