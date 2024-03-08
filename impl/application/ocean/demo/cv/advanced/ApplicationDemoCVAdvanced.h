65d447c756eb4f4e8c283ca5b08768cb99be2e34
Daniel Thornton <danthornton@meta.com>
2024-03-08 09:27:46 -0800

    Move demo/cv/advanced to //xplat/ocean/
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/cv/advanced` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/cv/advanced` to
    `xplat/ocean/impl/application/ocean/demo/cv/advanced`
    
    Differential Revision: D54644775
    
    fbshipit-source-id: 09737498536a8ce70d29a9c4db12dbb01300f061

---
+++ impl/application/ocean/demo/cv/advanced/ApplicationDemoCVAdvanced.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef APPLICATION_DEMO_CV_ADVANCED_H
+#define APPLICATION_DEMO_CV_ADVANCED_H
+
+#include "application/ocean/demo/cv/ApplicationDemoCV.h"
+
+/**
+ * @ingroup applicationdemocv
+ * @defgroup applicationdemocvadvanced Advanced Computer Vision demo applications
+ * @{
+ * All demo applications demonstrate some functionalities of the Ocean Advanced Computer Vision libraries.<br>
+ * @}
+ */
+
+#endif // APPLICATION_DEMO_CV_ADVANCED_H
---
+++ impl/application/ocean/demo/cv/advanced/poissonblending/wxw/PoissonBlendingApplication.h
@@ -0,0 +1,28 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_CV_ADVANCED_POISSONBLENDING_WXWIDGETS_POISSONBLENDING_APPLICATION_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_CV_ADVANCED_POISSONBLENDING_WXWIDGETS_POISSONBLENDING_APPLICATION_H
+
+#include "application/ocean/demo/cv/advanced/poissonblending/wxw/PoissonBlending.h"
+
+/**
+ * This class implements the application.
+ * @ingroup applicationdemocvPoissonBlendingwxw
+ */
+class PoissonBlendingApplication : public wxApp
+{
+	private:
+
+		/**
+		 * Initialization event function.
+		 * @return True, if succeeded
+		 */
+		bool OnInit() override;
+
+		/**
+		 * Application clean up.
+		 */
+		void CleanUp() override;
+};
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_CV_ADVANCED_POISSONBLENDING_WXWIDGETS_POISSONBLENDING_APPLICATION_H
