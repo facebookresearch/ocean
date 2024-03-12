f408f0d846f9bf95c20cc610e51e5be16b33b665
Daniel Thornton <danthornton@meta.com>
2024-03-12 15:05:23 -0700

    Move demo/rendering/opengles/ to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/rendering/opengles/` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/rendering/opengles/` to
    `xplat/ocean/impl/application/ocean/demo/rendering/opengles/`
    
    Also update demorenderingopengles_build.props with third party library path to fix runtime errors.
    
    Reviewed By: enpe
    
    Differential Revision: D54807533
    
    fbshipit-source-id: 7727df7d4f536ea94a1ad360fa1f226bfdff6a43

---
+++ impl/application/ocean/demo/rendering/opengles/win/OpenGLES.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_RENDERING_OPENGLES_WIN_MAIN_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_RENDERING_OPENGLES_WIN_MAIN_H
+
+#include "application/ocean/demo/rendering/ApplicationDemoRendering.h"
+
+/**
+ * @ingroup applicationdemorendering
+ * @defgroup applicationdemorenderingopengleswin OpenGLES (Windows)
+ * @{
+ * The demo application demonstrates the implementation of a simple OpenGL ES application.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_RENDERING_OPENGLES_WIN_MAIN_H
