4471dd8b73ddcef29de76ddd3225bb4ce268962b
Daniel Thornton <danthornton@meta.com>
2024-03-12 15:05:23 -0700

    Move demo/rendering/openglstereo to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/rendering/openglstereo/` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/rendering/openglstereo/` to
    `xplat/ocean/impl/application/ocean/demo/rendering/openglstereo/`
    
    Reviewed By: enpe
    
    Differential Revision: D54819370
    
    fbshipit-source-id: 3de5599d2454d9b63004bb347f456ad186e92ea5

---
+++ impl/application/ocean/demo/rendering/openglstereo/win/OpenGLStereo.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_RENDERING_OPENGL_STEREO_WIN_OPENGL_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_RENDERING_OPENGL_STEREO_WIN_OPENGL_H
+
+#include "application/ocean/demo/rendering/ApplicationDemoRendering.h"
+
+/**
+ * @ingroup applicationdemorendering
+ * @defgroup applicationdemorenderingopenglstereowin OpenGL Stereo (Windows)
+ * @{
+ * The demo application demonstrates the implementation of a simple OpenGL Stereo application.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_RENDERING_OPENGL_STEREO_WIN_OPENGL_H
