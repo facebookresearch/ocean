3583853ead436a38c7ca9941b89cb8cbe4d84341
Jan Herling <janherling@meta.com>
2024-03-13 11:09:39 -0700

    Minor code improvements in Triangle2
    Summary: Mainly cosmetric code improvements.
    
    Reviewed By: enpe
    
    Differential Revision: D54829573
    
    fbshipit-source-id: 1a2a267736398708162c82842f09ec9afa79fb01

--- impl/ocean/math/Triangle2.h
+++ impl/ocean/math/Triangle2.h
@@ -66,7 +66,7 @@ typedef std::vector<TriangleF2> TrianglesF2;
 typedef std::vector<TriangleD2> TrianglesD2;
 
 /**
- * This class implements a 2D triangle with cartesian coordinates.
+ * This class implements a 2D triangle with Cartesian coordinates.
  * @tparam T Data type used to represent coordinates
  * @see TriangleF2, TriangleD2, TriangleT3.
  * @ingroup math
@@ -79,7 +79,7 @@ class TriangleT2 : public TriangleT<T>
 		/**
 		 * Creates a new 2D triangle object with default parameters.
 		 */
-		inline TriangleT2();
+		TriangleT2() = default;
 
 		/**
 		 * Creates a new 2D triangle object by three corner positions.
@@ -222,15 +222,15 @@ class TriangleT2 : public TriangleT<T>
 
 		/**
 		 * Returns whether a given point lies inside this triangle.
-		 * @param point Point to be checked
+		 * @param point The point to be checked
 		 * @return True, if so
 		 */
 		inline bool isInside(const VectorT2<T>& point) const;
 
 		/**
 		 * Returns whether a given point lies inside at least one of the given triangles.
-		 * @param triangles Triangles that are tested
-		 * @param point Point to be checked
+		 * @param triangles The triangles that are tested
+		 * @param point The point to be checked
 		 * @return True, if so
 		 */
 		inline static bool isInside(const std::vector<TriangleT2<T>>& triangles, const VectorT2<T>& point);
@@ -254,15 +254,15 @@ class TriangleT2 : public TriangleT<T>
 		inline bool isCounterClockwise(const bool yAxisDownwards = true) const;
 
 		/**
-		 * Returns the 2D cartesian coordinate of a given barycentric coordinate defined in relation to this triangle.
-		 * @param barycentric Barycentric coordinate to convert to a cartesian coordinate
+		 * Returns the 2D Cartesian coordinate of a given barycentric coordinate defined in relation to this triangle.
+		 * @param barycentric The Barycentric coordinate to convert to a Cartesian coordinate
 		 * @return Cartesian coordinate
 		 */
 		inline VectorT2<T> barycentric2cartesian(const VectorT3<T>& barycentric) const;
 
 		/**
-		 * Returns the barycentric coordinate of a given 2D cartesian coordinate defined in relation to this triangle.
-		 * @param cartesian Cartesian coordinate to convert to a barycentric coordinate
+		 * Returns the barycentric coordinate of a given 2D Cartesian coordinate defined in relation to this triangle.
+		 * @param cartesian The Cartesian coordinate to convert to a barycentric coordinate
 		 * @return Barycentric coordinate
 		 */
 		inline VectorT3<T> cartesian2barycentric(const VectorT2<T>& cartesian) const;
@@ -274,7 +274,7 @@ class TriangleT2 : public TriangleT<T>
 		VectorT3<T> barycentricCircumcenter() const;
 
 		/**
-		 * Returns the circumcenter for this triangle in cartesian coordinates.
+		 * Returns the circumcenter for this triangle in Cartesian coordinates.
 		 * @return Cartesian coordinates of the circumcenter
 		 */
 		inline VectorT2<T> cartesianCircumcenter() const;
@@ -286,7 +286,7 @@ class TriangleT2 : public TriangleT<T>
 		VectorT3<T> barycentricIncenter() const;
 
 		/**
-		 * Returns the incenter for this triangle in cartesian coordinates.
+		 * Returns the incenter for this triangle in Cartesian coordinates.
 		 * @return Cartesian coordinates of the circumcenter
 		 */
 		inline VectorT2<T> cartesianIncenter() const;
@@ -371,100 +371,91 @@ class TriangleT2 : public TriangleT<T>
 	private:
 
 		/// The corner positions.
-		VectorT2<T> trianglePoints[3];
+		VectorT2<T> points_[3] = {VectorT2<T>(T(0), T(0)), VectorT2<T>(T(0), T(0)), VectorT2<T>(T(0), T(0))};
 
 		/// Convert factor for barycentric coordinates.
-		T triangleBarycentricFactor;
+		T barycentricFactor_ = T(0);
 };
 
 template <typename T>
-inline TriangleT2<T>::TriangleT2() :
-	triangleBarycentricFactor(T(0))
-{
-	trianglePoints[0] = VectorT2<T>(T(0), T(0));
-	trianglePoints[1] = VectorT2<T>(T(0), T(0));
-	trianglePoints[2] = VectorT2<T>(T(0), T(0));
-}
-
-template <typename T>
 inline TriangleT2<T>::TriangleT2(const VectorT2<T>& point0, const VectorT2<T>& point1, const VectorT2<T>& point2) :
-	triangleBarycentricFactor(T(0))
+	barycentricFactor_(T(0))
 {
-	trianglePoints[0] = point0;
-	trianglePoints[1] = point1;
-	trianglePoints[2] = point2;
+	points_[0] = point0;
+	points_[1] = point1;
+	points_[2] = point2;
 
-	const T factor = (trianglePoints[1].y() - trianglePoints[2].y()) * (trianglePoints[0].x() - trianglePoints[2].x())
-								+ (trianglePoints[2].x() - trianglePoints[1].x()) * (trianglePoints[0].y() - trianglePoints[2].y());
+	const T factor = (points_[1].y() - points_[2].y()) * (points_[0].x() - points_[2].x())
+								+ (points_[2].x() - points_[1].x()) * (points_[0].y() - points_[2].y());
 
 	if (NumericT<T>::isNotEqualEps(factor))
 	{
-		triangleBarycentricFactor = T(1) / factor;
+		barycentricFactor_ = T(1) / factor;
 	}
 }
 
 template <typename T>
 inline const VectorT2<T>& TriangleT2<T>::point0() const
 {
-	return trianglePoints[0];
+	return points_[0];
 }
 
 template <typename T>
 inline const VectorT2<T>& TriangleT2<T>::point1() const
 {
-	return trianglePoints[1];
+	return points_[1];
 }
 
 template <typename T>
 inline const VectorT2<T>& TriangleT2<T>::point2() const
 {
-	return trianglePoints[2];
+	return points_[2];
 }
 
 template <typename T>
 inline T TriangleT2<T>::sqrDistance01() const
 {
-	return trianglePoints[0].sqrDistance(trianglePoints[1]);
+	return points_[0].sqrDistance(points_[1]);
 }
 
 template <typename T>
 inline T TriangleT2<T>::sqrDistance02() const
 {
-	return trianglePoints[0].sqrDistance(trianglePoints[2]);
+	return points_[0].sqrDistance(points_[2]);
 }
 
 template <typename T>
 inline T TriangleT2<T>::sqrDistance12() const
 {
-	return trianglePoints[1].sqrDistance(trianglePoints[2]);
+	return points_[1].sqrDistance(points_[2]);
 }
 
 template <typename T>
 inline T TriangleT2<T>::left() const
 {
 	ocean_assert(isValid());
-	return min(trianglePoints[0].x(), min(trianglePoints[1].x(), trianglePoints[2].x()));
+	return min(points_[0].x(), min(points_[1].x(), points_[2].x()));
 }
 
 template <typename T>
 inline T TriangleT2<T>::top() const
 {
 	ocean_assert(isValid());
-	return min(trianglePoints[0].y(), min(trianglePoints[1].y(), trianglePoints[2].y()));
+	return min(points_[0].y(), min(points_[1].y(), points_[2].y()));
 }
 
 template <typename T>
 inline T TriangleT2<T>::right() const
 {
 	ocean_assert(isValid());
-	return max(trianglePoints[0].x(), max(trianglePoints[1].x(), trianglePoints[2].x()));
+	return max(points_[0].x(), max(points_[1].x(), points_[2].x()));
 }
 
 template <typename T>
 inline T TriangleT2<T>::bottom() const
 {
 	ocean_assert(isValid());
-	return max(trianglePoints[0].y(), max(trianglePoints[1].y(), trianglePoints[2].y()));
+	return max(points_[0].y(), max(points_[1].y(), points_[2].y()));
 }
 
 template <typename T>
@@ -486,9 +477,9 @@ inline T TriangleT2<T>::area() const
 template <typename T>
 inline T TriangleT2<T>::area2() const
 {
-	const T a2 = trianglePoints[0].sqrDistance(trianglePoints[1]);
-	const T b2 = trianglePoints[0].sqrDistance(trianglePoints[2]);
-	const T c2 = trianglePoints[1].sqrDistance(trianglePoints[2]);
+	const T a2 = points_[0].sqrDistance(points_[1]);
+	const T b2 = points_[0].sqrDistance(points_[2]);
+	const T c2 = points_[1].sqrDistance(points_[2]);
 
 	return (T(4) * a2 * c2 - NumericT<T>::sqr(a2 + c2 - b2)) * T(0.0625);
 }
@@ -498,9 +489,9 @@ void TriangleT2<T>::cosines(T& cosine0, T& cosine1, T& cosine2) const
 {
 	ocean_assert(isValid());
 
-	const T sqrDistance01(trianglePoints[1].sqrDistance(trianglePoints[0]));
-	const T sqrDistance02(trianglePoints[2].sqrDistance(trianglePoints[0]));
-	const T sqrDistance12(trianglePoints[2].sqrDistance(trianglePoints[1]));
+	const T sqrDistance01(points_[1].sqrDistance(points_[0]));
+	const T sqrDistance02(points_[2].sqrDistance(points_[0]));
+	const T sqrDistance12(points_[2].sqrDistance(points_[1]));
 
 	const T factorDistance01(T(1) / NumericT<T>::sqrt(sqrDistance01));
 	const T factorDistance02(T(1) / NumericT<T>::sqrt(sqrDistance02));
@@ -521,9 +512,9 @@ bool TriangleT2<T>::allCosineBelow(const T cosValue) const
 {
 	ocean_assert(isValid());
 
-	const T sqrDistance01(trianglePoints[1].sqrDistance(trianglePoints[0]));
-	const T sqrDistance02(trianglePoints[2].sqrDistance(trianglePoints[0]));
-	const T sqrDistance12(trianglePoints[2].sqrDistance(trianglePoints[1]));
+	const T sqrDistance01(points_[1].sqrDistance(points_[0]));
+	const T sqrDistance02(points_[2].sqrDistance(points_[0]));
+	const T sqrDistance12(points_[2].sqrDistance(points_[1]));
 
 	const T factorDistance01(T(1) / NumericT<T>::sqrt(sqrDistance01));
 	const T factorDistance02(T(1) / NumericT<T>::sqrt(sqrDistance02));
@@ -542,9 +533,9 @@ VectorT3<T> TriangleT2<T>::barycentricCircumcenter() const
 {
 	ocean_assert(isValid());
 
-	const T a2 = trianglePoints[1].sqrDistance(trianglePoints[2]);
-	const T b2 = trianglePoints[0].sqrDistance(trianglePoints[2]);
-	const T c2 = trianglePoints[0].sqrDistance(trianglePoints[1]);
+	const T a2 = points_[1].sqrDistance(points_[2]);
+	const T b2 = points_[0].sqrDistance(points_[2]);
+	const T c2 = points_[0].sqrDistance(points_[1]);
 
 	const T coord0 = a2 * (-a2 + b2 + c2);
 	const T coord1 = b2 * (a2 - b2 + c2);
@@ -563,9 +554,9 @@ VectorT3<T> TriangleT2<T>::barycentricIncenter() const
 {
 	ocean_assert(isValid());
 
-	const T a = trianglePoints[1].distance(trianglePoints[2]);
-	const T b = trianglePoints[0].distance(trianglePoints[2]);
-	const T c = trianglePoints[0].distance(trianglePoints[1]);
+	const T a = points_[1].distance(points_[2]);
+	const T b = points_[0].distance(points_[2]);
+	const T c = points_[0].distance(points_[1]);
 
 	const T total = a + b + c;
 	ocean_assert(NumericT<T>::isNotEqualEps(total));
@@ -627,9 +618,9 @@ TriangleT2<T> TriangleT2<T>::padded(const T padWidth) const
 	}
 
 	// Create homogeneous 2D points.
-	const VectorT3<T> hPoint0(trianglePoints[0], T(1.0));
-	const VectorT3<T> hPoint1(trianglePoints[1], T(1.0));
-	const VectorT3<T> hPoint2(trianglePoints[2], T(1.0));
+	const VectorT3<T> hPoint0(points_[0], T(1.0));
+	const VectorT3<T> hPoint1(points_[1], T(1.0));
+	const VectorT3<T> hPoint2(points_[2], T(1.0));
 
 	VectorT3<T> line01 = hPoint0.cross(hPoint1);
 	VectorT3<T> line12 = hPoint1.cross(hPoint2);
@@ -701,9 +692,9 @@ inline T TriangleT2<T>::minAngle() const
 template <typename T>
 inline T TriangleT2<T>::maxSqrLength() const
 {
-	const T sqrLength01(trianglePoints[0].sqrDistance(trianglePoints[1]));
-	const T sqrLength02(trianglePoints[0].sqrDistance(trianglePoints[2]));
-	const T sqrLength12(trianglePoints[1].sqrDistance(trianglePoints[2]));
+	const T sqrLength01(points_[0].sqrDistance(points_[1]));
+	const T sqrLength02(points_[0].sqrDistance(points_[2]));
+	const T sqrLength12(points_[1].sqrDistance(points_[2]));
 
 	return max(sqrLength01, max(sqrLength02, sqrLength12));
 }
@@ -717,9 +708,9 @@ inline T TriangleT2<T>::maxLength() const
 template <typename T>
 inline T TriangleT2<T>::minSqrLength() const
 {
-	const T sqrLength01(trianglePoints[0].sqrDistance(trianglePoints[1]));
-	const T sqrLength02(trianglePoints[0].sqrDistance(trianglePoints[2]));
-	const T sqrLength12(trianglePoints[1].sqrDistance(trianglePoints[2]));
+	const T sqrLength01(points_[0].sqrDistance(points_[1]));
+	const T sqrLength02(points_[0].sqrDistance(points_[2]));
+	const T sqrLength12(points_[1].sqrDistance(points_[2]));
 
 	return min(sqrLength01, min(sqrLength02, sqrLength12));
 }
@@ -727,7 +718,7 @@ inline T TriangleT2<T>::minSqrLength() const
 template <typename T>
 inline T TriangleT2<T>::minLength() const
 {
-	return Numeric::sqrt(minSqrLength());
+	return NumericT<T>::sqrt(minSqrLength());
 }
 
 template <typename T>
@@ -755,7 +746,7 @@ inline bool TriangleT2<T>::isCounterClockwise(const bool yAxisDownwards) const
 {
 	ocean_assert(isValid());
 
-	return analyzePoints(trianglePoints[0], trianglePoints[1], trianglePoints[2], yAxisDownwards) < T(0);
+	return analyzePoints(points_[0], points_[1], points_[2], yAxisDownwards) < T(0);
 }
 
 template <typename T>
@@ -763,8 +754,8 @@ inline VectorT2<T> TriangleT2<T>::barycentric2cartesian(const VectorT3<T>& baryc
 {
 	ocean_assert_accuracy((std::is_same<float, T>::value || TriangleT<T>::isValidBarycentric(barycentric, NumericT<T>::weakEps())));
 
-	return VectorT2<T>((trianglePoints[0].x() * barycentric[0] + trianglePoints[1].x() * barycentric[1] + trianglePoints[2].x() * barycentric[2]),
-						(trianglePoints[0].y() * barycentric[0] + trianglePoints[1].y() * barycentric[1] + trianglePoints[2].y() * barycentric[2]));
+	return VectorT2<T>((points_[0].x() * barycentric[0] + points_[1].x() * barycentric[1] + points_[2].x() * barycentric[2]),
+						(points_[0].y() * barycentric[0] + points_[1].y() * barycentric[1] + points_[2].y() * barycentric[2]));
 }
 
 template <typename T>
@@ -772,11 +763,11 @@ inline VectorT3<T> TriangleT2<T>::cartesian2barycentric(const VectorT2<T>& carte
 {
 	ocean_assert(isValid());
 
-	const T barycentric0 = ((trianglePoints[1].y() - trianglePoints[2].y()) * (cartesian.x() - trianglePoints[2].x())
-							+ (trianglePoints[2].x() - trianglePoints[1].x()) * (cartesian.y() -trianglePoints[2].y())) * triangleBarycentricFactor;
+	const T barycentric0 = ((points_[1].y() - points_[2].y()) * (cartesian.x() - points_[2].x())
+							+ (points_[2].x() - points_[1].x()) * (cartesian.y() -points_[2].y())) * barycentricFactor_;
 
-	const T barycentric1 = ((trianglePoints[2].y() - trianglePoints[0].y()) * (cartesian.x() - trianglePoints[2].x())
-							+ (trianglePoints[0].x() - trianglePoints[2].x()) * (cartesian.y() - trianglePoints[2].y())) * triangleBarycentricFactor;
+	const T barycentric1 = ((points_[2].y() - points_[0].y()) * (cartesian.x() - points_[2].x())
+							+ (points_[0].x() - points_[2].x()) * (cartesian.y() - points_[2].y())) * barycentricFactor_;
 
 #ifdef OCEAN_DEBUG
 	if (std::is_same<float, Scalar>::value)
@@ -811,11 +802,11 @@ inline bool TriangleT2<T>::isValid() const
 {
 	if (std::is_same<T, double>::value)
 	{
-		return NumericT<T>::isNotEqualEps(triangleBarycentricFactor);
+		return NumericT<T>::isNotEqualEps(barycentricFactor_);
 	}
 	else
 	{
-		return trianglePoints[0] != trianglePoints[1] && trianglePoints[0] != trianglePoints[2] && trianglePoints[1] != trianglePoints[2];
+		return points_[0] != points_[1] && points_[0] != points_[2] && points_[1] != points_[2];
 	}
 }
 
@@ -823,21 +814,21 @@ template <typename T>
 inline const VectorT2<T>& TriangleT2<T>::operator[](const unsigned int index) const
 {
 	ocean_assert(index <= 2u);
-	return trianglePoints[index];
+	return points_[index];
 }
 
 template <typename T>
 inline TriangleT2<T> TriangleT2<T>::operator+(const VectorT2<T>& offset) const
 {
-	return TriangleT2<T>(trianglePoints[0] + offset, trianglePoints[1] + offset, trianglePoints[2] + offset);
+	return TriangleT2<T>(points_[0] + offset, points_[1] + offset, points_[2] + offset);
 }
 
 template <typename T>
 inline TriangleT2<T>& TriangleT2<T>::operator+=(const VectorT2<T>& offset)
 {
-	trianglePoints[0] += offset;
-	trianglePoints[1] += offset;
-	trianglePoints[2] += offset;
+	points_[0] += offset;
+	points_[1] += offset;
+	points_[2] += offset;
 
 	return *this;
 }
@@ -845,15 +836,15 @@ inline TriangleT2<T>& TriangleT2<T>::operator+=(const VectorT2<T>& offset)
 template <typename T>
 inline TriangleT2<T> TriangleT2<T>::operator-(const VectorT2<T>& offset) const
 {
-	return TriangleT2<T>(trianglePoints[0] - offset, trianglePoints[1] - offset, trianglePoints[2] - offset);
+	return TriangleT2<T>(points_[0] - offset, points_[1] - offset, points_[2] - offset);
 }
 
 template <typename T>
 inline TriangleT2<T>& TriangleT2<T>::operator-=(const VectorT2<T>& offset)
 {
-	trianglePoints[0] -= offset;
-	trianglePoints[1] -= offset;
-	trianglePoints[2] -= offset;
+	points_[0] -= offset;
+	points_[1] -= offset;
+	points_[2] -= offset;
 
 	return *this;
 }
