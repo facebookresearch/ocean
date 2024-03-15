c2b3baaca0c648f02d5d762be35bee6cd88f4096
Daniel Thornton <danthornton@meta.com>
2024-03-15 11:06:04 -0700

    Move demo/tracking/sphericalenvironment to //xplat/ocean
    Summary:
    Move contents of
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/tracking/sphericalenvironment/` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/tracking/sphericalenvironment/` to
    `xplat/ocean/impl/metaonly/application/ocean/demo/tracking/sphericalenvironment/`
    
    Reviewed By: enpe
    
    Differential Revision: D54930197
    
    fbshipit-source-id: 2e7ff62586a3ca1ac178c2244791a2da6e2f4f68

---
+++ impl/application/ocean/demo/tracking/sphericalenvironment/win/SEApplication.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_SPHERICAL_ENVIORNMENT_WIN_SE_APPLICATION_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_SPHERICAL_ENVIORNMENT_WIN_SE_APPLICATION_H
+
+#include "application/ocean/demo/tracking/ApplicationDemoTracking.h"
+
+/**
+ * @ingroup applicationdemotracking
+ * @defgroup applicationdemotrackingsphericalenvironmentwin Spherical Enviornment (Windows)
+ * @{
+ * The demo application demonstrates the usage of the Spherical Environment class.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_SPHERICAL_ENVIORNMENT_WIN_SE_APPLICATION_H
