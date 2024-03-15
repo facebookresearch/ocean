5d0cc49fe4568fac3dd61f9f5f6447827931235c
Paul Lee <paullee23@meta.com>
2024-03-15 16:18:49 -0700

    Move tracking/offline demo applications to "xplat/ocean/"
    Reviewed By: enpe
    
    Differential Revision: D54927117
    
    fbshipit-source-id: 69f202816e5261fe5a091c059114213c264defc7

---
+++ impl/application/ocean/demo/tracking/offline/ApplicationDemoTrackingOffline.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef APPLICATION_DEMO_TRACKING_OFFLINE_H
+#define APPLICATION_DEMO_TRACKING_OFFLINE_H
+
+#include "application/ocean/demo/tracking/ApplicationDemoTracking.h"
+
+/**
+ * @ingroup applicationdemotracking
+ * @defgroup applicationdemotrackingoffline Offline Tracking demo applications
+ * @{
+ * All demo applications demonstrates some functionalities of the Ocean Offline Tracking libraries.<br>
+ * @}
+ */
+
+#endif // APPLICATION_DEMO_TRACKING_OFFLINE_H
---
+++ impl/application/ocean/demo/tracking/offline/structurefrommotion/win/SFMApplication.h
@@ -0,0 +1,19 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_OFFLINE_STRUCTURE_FROM_MOTION_WIN_SFM_APPLICATION_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_OFFLINE_STRUCTURE_FROM_MOTION_WIN_SFM_APPLICATION_H
+
+#include "application/ocean/demo/tracking/offline/ApplicationDemoTrackingOffline.h"
+
+/**
+ * @ingroup applicationdemotrackingoffline
+ * @defgroup applicationdemotrackingofflinestructurefrommotionwin Structure from Motion (Windows)
+ * @{
+ * The demo application demonstrates the usage of several classes realizing a Structure from Motion application.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+using namespace Ocean;
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_OFFLINE_STRUCTURE_FROM_MOTION_WIN_SFM_APPLICATION_H
