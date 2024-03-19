e7025d7dbe38d115ad6a55decc6af885d6635b54
Jan Herling <janherling@meta.com>
2024-03-19 09:14:05 -0700

    Improved test for Plan3
    Summary:
    Trying to address 32bit precision issues.
    If this does not fix the test, we need to sperate the test into individual sub-tests to see which test is causing the problem.
    
    Reviewed By: enpe
    
    Differential Revision: D55044054
    
    fbshipit-source-id: b65304cedcaca2bffe9ac8f31cc600196ae4a860

--- impl/ocean/test/testmath/TestPlane3.cpp
+++ impl/ocean/test/testmath/TestPlane3.cpp
@@ -78,8 +78,8 @@ bool TestPlane3::testConstructorThreePoints(const double testDuration)
 
 	Log::info() << "Three point constructor test:";
 
-	unsigned long long iterations = 0ull;
-	unsigned long long succeeded = 0ull;
+	uint64_t iterations = 0ull;
+	uint64_t succeeded = 0ull;
 
 	const Timestamp startTimestamp(true);
 
@@ -87,55 +87,71 @@ bool TestPlane3::testConstructorThreePoints(const double testDuration)
 	{
 		for (unsigned int n = 0u; n < 1000u; ++n)
 		{
+			constexpr Scalar range = std::is_same<Scalar, float>::value ? Scalar(10) : Scalar(100);
+
 			// all points identical
-			const Vector3 objectPoint(Random::vector3(-100, 100));
+			const Vector3 objectPoint(Random::vector3(-range, range));
 
 			const Plane3 plane(objectPoint, objectPoint, objectPoint);
 
 			if (!plane.isValid())
-				succeeded++;
+			{
+				++succeeded;
+			}
 
 			iterations++;
 		}
 
 		for (unsigned int n = 0u; n < 1000u; ++n)
 		{
+			constexpr Scalar range = std::is_same<Scalar, float>::value ? Scalar(10) : Scalar(100);
+
 			// first and second point identical
-			const Vector3 objectPoint0(Random::vector3(-100, 100));
-			const Vector3 objectPoint1(Random::vector3(-100, 100));
+			const Vector3 objectPoint0(Random::vector3(-range, range));
+			const Vector3 objectPoint1(Random::vector3(-range, range));
 
 			const Plane3 plane(objectPoint0, objectPoint0, objectPoint1);
 
 			if (!plane.isValid())
-				succeeded++;
+			{
+				++succeeded;
+			}
 
 			iterations++;
 		}
 
 		for (unsigned int n = 0u; n < 1000u; ++n)
 		{
+			constexpr Scalar range = std::is_same<Scalar, float>::value ? Scalar(10) : Scalar(100);
+
 			// first and third point identical
-			const Vector3 objectPoint0(Random::vector3(-100, 100));
-			const Vector3 objectPoint1(Random::vector3(-100, 100));
+			const Vector3 objectPoint0(Random::vector3(-range, range));
+			const Vector3 objectPoint1(Random::vector3(-range, range));
 
 			const Plane3 plane(objectPoint0, objectPoint1, objectPoint0);
 
 			if (!plane.isValid())
-				succeeded++;
+			{
+				++succeeded;
+			}
 
 			iterations++;
 		}
 
 		for (unsigned int n = 0u; n < 1000u; ++n)
 		{
+			constexpr Scalar range = std::is_same<Scalar, float>::value ? Scalar(10) : Scalar(100);
+
 			// second and third point identical
-			const Vector3 objectPoint0(Random::vector3(-100, 100));
-			const Vector3 objectPoint1(Random::vector3(-100, 100));
+			const Vector3 objectPoint0(Random::vector3(-range, range));
+			const Vector3 objectPoint1(Random::vector3(-range, range));
 
 			const Plane3 plane(objectPoint0, objectPoint1, objectPoint1);
 
 			if (!plane.isValid())
-				succeeded++;
+			{
+				++succeeded;
+			}
 
 			iterations++;
 		}
@@ -143,14 +159,18 @@ bool TestPlane3::testConstructorThreePoints(const double testDuration)
 		for (unsigned int n = 0u; n < 1000u; ++n)
 		{
 			// we explicitly use a smaller region [-1, 1] or [-10, 10] to ensure that we really receive collinear points
-			const Scalar range = std::is_same<Scalar, float>::value ? Scalar(1) : Scalar(10);
+			constexpr Scalar range = std::is_same<Scalar, float>::value ? Scalar(1) : Scalar(10);
+			constexpr Scalar equalEps = std::is_same<Scalar, float>::value ? Scalar(0.01) : Numeric::weakEps();
 
 			// non-collinear test
 			const Vector3 objectPoint0(Random::vector3(-range, range));
+
 			Vector3 objectPoint1(Random::vector3(-range, range));
 
-			while (objectPoint0 == objectPoint1)
+			while (objectPoint0.isEqual(objectPoint1, equalEps))
+			{
 				objectPoint1 = Random::vector3(-range, range);
+			}
 
 			const Line3 line(objectPoint0, (objectPoint1 - objectPoint0).normalized());
 
@@ -159,7 +179,9 @@ bool TestPlane3::testConstructorThreePoints(const double testDuration)
 			const Plane3 plane(objectPoint0, objectPoint1, objectPoint2);
 
 			if (!plane.isValid())
-				succeeded++;
+			{
+				++succeeded;
+			}
 
 			iterations++;
 		}
@@ -167,7 +189,8 @@ bool TestPlane3::testConstructorThreePoints(const double testDuration)
 		for (unsigned int n = 0u; n < 1000u; ++n)
 		{
 			// we explicitly use a smaller region [-5, 5] or [-100, 100] to ensure that we really receive collinear points
-			const Scalar range = std::is_same<Scalar, float>::value ? Scalar(5) : Scalar(100);
+			constexpr Scalar range = std::is_same<Scalar, float>::value ? Scalar(5) : Scalar(100);
+			constexpr Scalar equalEps = std::is_same<Scalar, float>::value ? Scalar(0.01) : Numeric::weakEps();
 
 			const Vector3 objectPoint0(Random::vector3(-range, range));
 			const Vector3 objectPoint1(Random::vector3(-range, range));
@@ -175,12 +198,14 @@ bool TestPlane3::testConstructorThreePoints(const double testDuration)
 
 			const Plane3 plane(objectPoint0, objectPoint1, objectPoint2);
 
-			if (objectPoint0 != objectPoint1 && objectPoint0 != objectPoint2 && objectPoint1 != objectPoint2)
+			if (!objectPoint0.isEqual(objectPoint1, equalEps) && !objectPoint0.isEqual(objectPoint2, equalEps) && !objectPoint1.isEqual(objectPoint2, equalEps))
 			{
 				if (Numeric::isNotEqual(Numeric::abs((objectPoint1 - objectPoint0).normalized() * (objectPoint2 - objectPoint0).normalized()), Scalar(1)))
 				{
 					if (plane.isInPlane(objectPoint0) && plane.isInPlane(objectPoint1) && plane.isInPlane(objectPoint2))
-						succeeded++;
+					{
+						++succeeded;
+					}
 
 					++iterations;
 				}
@@ -203,8 +228,8 @@ bool TestPlane3::testIntersectionLine(const double testDuration)
 
 	Log::info() << "Plane-line intersection test:";
 
-	unsigned long long iterations = 0ull;
-	unsigned long long succeeded = 0ull;
+	uint64_t iterations = 0ull;
+	uint64_t succeeded = 0ull;
 
 	const Timestamp startTimestamp(true);
 
@@ -223,7 +248,7 @@ bool TestPlane3::testIntersectionLine(const double testDuration)
 
 				if (Numeric::isWeakEqualEps(d0) && Numeric::isWeakEqualEps(d1))
 				{
-					succeeded++;
+					++succeeded;
 				}
 			}
 			else
@@ -232,7 +257,7 @@ bool TestPlane3::testIntersectionLine(const double testDuration)
 
 				if (Numeric::isEqualEps(plane.normal() * line.direction()))
 				{
-					succeeded++;
+					++succeeded;
 				}
 			}
 
@@ -255,8 +280,8 @@ bool TestPlane3::testIntersectionPlane(const double testDuration)
 
 	Log::info() << "Plane-plane intersection test:";
 
-	unsigned long long iterations = 0ull;
-	unsigned long long succeeded = 0ull;
+	uint64_t iterations = 0ull;
+	uint64_t succeeded = 0ull;
 
 	const Timestamp startTimestamp(true);
 
@@ -281,7 +306,7 @@ bool TestPlane3::testIntersectionPlane(const double testDuration)
 				if (Numeric::isWeakEqualEps(distanceA0) && Numeric::isWeakEqualEps(distanceA1) && Numeric::isWeakEqualEps(distanceA2)
 						&& Numeric::isWeakEqualEps(distanceB0) && Numeric::isWeakEqualEps(distanceB1) && Numeric::isWeakEqualEps(distanceB2))
 				{
-					succeeded++;
+					++succeeded;
 				}
 			}
 			else
@@ -290,7 +315,7 @@ bool TestPlane3::testIntersectionPlane(const double testDuration)
 
 				if (planeA.normal().isParallel(planeB.normal()))
 				{
-					succeeded++;
+					++succeeded;
 				}
 			}
 
