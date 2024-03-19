d2446fa03abb049bc3dd39768337735fc0d4a66a
Daniel Thornton <danthornton@meta.com>
2024-03-19 15:51:14 -0700

    Move demo/geometry/ to //xplat/ocean
    Summary:
    Move
    `xplat/compphoto/ocean/impl/h/application/ocean/demo/geometry/` and
    `xplat/compphoto/ocean/impl/src/application/ocean/demo/geometry/` to
    `xplat/ocean/impl/application/ocean/demo/geometry/`
    
    Update source/header file paths for visual studio projects
    
    Reviewed By: enpe
    
    Differential Revision: D55076263
    
    fbshipit-source-id: c39d50c58d02879c238d89c1924a57a8974c112c

---
+++ impl/application/ocean/demo/geometry/ApplicationDemoGeometry.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef APPLICATION_DEMO_GEOMETRY_H
+#define APPLICATION_DEMO_GEOMETRY_H
+
+#include "application/ocean/demo/ApplicationDemo.h"
+
+/**
+ * @ingroup applicationdemo
+ * @defgroup applicationdemogeometry Geometry demo applications
+ * @{
+ * All demo applications demonstrates some functionalities of the Ocean Geometry library.<br>
+ * @}
+ */
+
+#endif // APPLICATION_DEMO_GEOMETRY_H
---
+++ impl/application/ocean/demo/geometry/vanishingprojection/win/VanishingProjection.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_GEOMETRY_VANISHING_PROJECTION_WIN_VANISHING_PROJECTION_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_GEOMETRY_VANISHING_PROJECTION_WIN_VANISHING_PROJECTION_H
+
+#include "application/ocean/demo/geometry/ApplicationDemoGeometry.h"
+
+/**
+ * @ingroup applicationdemogeometry
+ * @defgroup applicationdemogeometryvanishingprojectionwin Vanishing Projection (Windows)
+ * @{
+ * The demo application demonstrates the usage of some vanishing projection functions.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_GEOMETRY_VANISHING_PROJECTION_WIN_VANISHING_PROJECTION_H
