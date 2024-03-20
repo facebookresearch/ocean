c296047d1b653705c42fb36852aa61edd4c79572
Daniel Thornton <danthornton@meta.com>
2024-03-20 10:15:35 -0700

    Move demo/io to xplat/ocean
    Summary: Move `//xplat/compphoto/ocean/impl/{h,src}/application/ocean/demo/io` to `//xplat/ocean/impl/application/ocean/demo/io`
    
    Reviewed By: enpe
    
    Differential Revision: D55091149
    
    fbshipit-source-id: dd7dc13dbd529a9636f2f6fbfc69259fcecd9e5b

---
+++ impl/application/ocean/demo/io/ApplicationDemoIO.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef APPLICATION_DEMO_IO_H
+#define APPLICATION_DEMO_IO_H
+
+#include "application/ocean/demo/ApplicationDemo.h"
+
+/**
+ * @ingroup applicationdemo
+ * @defgroup applicationdemoio IO demo applications
+ * @{
+ * All demo applications demonstrates some functionalities of the Ocean IO library.<br>
+ * @}
+ */
+
+#endif // APPLICATION_DEMO_IO_H
---
+++ impl/application/ocean/demo/io/cameracalibrationmanager/CameraCalibrationManager.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_IO_CAMERA_CALIBRATION_MANAGER_CAMERA_CALIBRATION_MANAGER_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_IO_CAMERA_CALIBRATION_MANAGER_CAMERA_CALIBRATION_MANAGER_H
+
+#include "application/ocean/demo/io/ApplicationDemoIO.h"
+
+/**
+ * @ingroup applicationdemoio
+ * @defgroup applicationdemoiocameracalibration Camera Calibration Manager
+ * @{
+ * The demo application demonstrates the implementation of the camera calibration manager.<br>
+ * This application is almost platform independent and is available on desktop platforms like e.g., Windows or OS X.
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_IO_CAMERA_CALIBRATION_MANAGER_CAMERA_CALIBRATION_MANAGER_H
