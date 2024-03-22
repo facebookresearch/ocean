00669928eae297f1fcc3dbdc07c22b0fd5440e4f
Paul Lee <paullee23@meta.com>
2024-03-22 16:02:42 -0700

    Move rmvtracker demo application to 'xplat/ocean/'
    Reviewed By: enpe
    
    Differential Revision: D55220308
    
    fbshipit-source-id: da0e0c7554d74bd2db7b75ad7bc8e5bc75b6cf15

---
+++ impl/application/ocean/demo/tracking/rmvtracker/win/RMVTracker.h
@@ -0,0 +1,17 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_RMV_TRACKER_WIN_RMV_TRACKER_H
+#define FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_RMV_TRACKER_WIN_RMV_TRACKER_H
+
+#include "application/ocean/demo/tracking/ApplicationDemoTracking.h"
+
+/**
+ * @ingroup applicationdemotracking
+ * @defgroup applicationdemotrackingrmvtrackerwin RMV Tracker (Windows)
+ * @{
+ * The demo application demonstrates the usage of the 6DOF RMV feature tracker.<br>
+ * This application is platform dependent and is implemented for windows platforms.<br>
+ * @}
+ */
+
+#endif // FACEBOOK_APPLICATION_OCEAN_DEMO_TRACKING_RMV_TRACKER_WIN_RMV_TRACKER_H
