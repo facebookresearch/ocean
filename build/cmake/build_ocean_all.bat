a1dc885b1e66ad08c842aa3b3330b632c29723d6
Nils Plath <nilsplath@meta.com>
2024-04-02 16:20:01 -0700

    Restrict build script for CMake on Windows to Debug and Release builds
    Summary: By default CMake also includes "RelWithDebSymbols" and "MinSize" (their names are actually a bit different). But we only need "Debug" and "Release" for our purposes.
    
    Reviewed By: ASchneiderMeta
    
    Differential Revision: D55660536
    
    fbshipit-source-id: 330d7146263f2767df300135565e7e6dc98d2033

--- build/cmake/build_ocean_all.bat
+++ build/cmake/build_ocean_all.bat
@@ -4,13 +4,13 @@
 set OCEAN_SOURCE_DIR=%~dp0..\..
 
 @REM Static, debug + release
-cmake -S %OCEAN_SOURCE_DIR% -B C:\tmp\ocean_build_static -DCMAKE_INSTALL_PREFIX=C:\tmp\ocean_install_static -DOCEAN_BUILD_THIRD_PARTY_LIBS=TRUE
+cmake -S %OCEAN_SOURCE_DIR% -B C:\tmp\ocean_build_static -DCMAKE_INSTALL_PREFIX=C:\tmp\ocean_install_static -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -DOCEAN_BUILD_THIRD_PARTY_LIBS=TRUE
 
 cmake --build C:\tmp\ocean_build_static --config Release --target install -- /m:16
 cmake --build C:\tmp\ocean_build_static --config Debug --target install -- /m:16
 
 @REM Shared, debug + release
-cmake -S %OCEAN_SOURCE_DIR% -B C:\tmp\ocean_build_shared -DCMAKE_INSTALL_PREFIX=C:\tmp\ocean_install_shared -DBUILD_SHARED_LIBS=ON -DOCEAN_BUILD_THIRD_PARTY_LIBS=TRUE
+cmake -S %OCEAN_SOURCE_DIR% -B C:\tmp\ocean_build_shared -DCMAKE_INSTALL_PREFIX=C:\tmp\ocean_install_shared -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -DBUILD_SHARED_LIBS=ON -DOCEAN_BUILD_THIRD_PARTY_LIBS=TRUE
 
 cmake --build C:\tmp\ocean_build_shared --config Release --target install -- /m:16
 cmake --build C:\tmp\ocean_build_shared --config Debug --target install -- /m:16
