1b4703e75b1ca358afde43c32bc3f57d45a6f3b3
Nils Plath <nilsplath@meta.com>
2024-04-04 15:11:31 -0700

    Improve build script for the third-party libraries
    Summary: Refactored the script to make better re-use of the same calls.
    
    Reviewed By: ASchneiderMeta
    
    Differential Revision: D55767968
    
    fbshipit-source-id: fa5e6ba5d0bb68666d4bbf0d8be8b5c153b47603

--- build/cmake/build_third-party.sh
+++ build/cmake/build_third-party.sh
@@ -4,44 +4,57 @@
 echo "Building the third-party libraries required for Ocean ...:"
 echo " "
 
-OCEAN_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../../build/cmake/third-party && pwd )
+OCEAN_THIRD_PARTY_SOURCE_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../../build/cmake/third-party && pwd )
+OCEAN_THIRD_PARTY_BUILD_ROOT_DIRECTORY="/tmp"
+OCEAN_THIRD_PARTY_INSTALL_ROOT_DIRECTORY="/tmp"
 
-echo "Static, release"
-echo " "
-INSTALL_DIRECTORY="/tmp/ocean_install_static_release"
-echo "Installation directory: ${INSTALL_DIRECTORY}"
-cmake -S "${OCEAN_DIRECTORY}" -B /tmp/ocean_third-party_build_static_release -DCMAKE_INSTALL_PREFIX="${INSTALL_DIRECTORY}" -DCMAKE_BUILD_TYPE=Release
-cmake --build /tmp/ocean_third-party_build_static_release --target install -- -j16
-echo " "
-echo " "
-echo " "
+# Builds the third-party libraries for Ocean (Linux & macOS)
+#
+# BUILD_TYPE: The build type to be used, valid values: Debug, Release
+# LIBRARY_TYPE: The type of libraries to be built, valid values: static, shared
+function run_build {
+    BUILD_TYPE=$1
+    if [[ ${BUILD_TYPE} != "Debug" ]] && [[ ${BUILD_TYPE} != "Release" ]]; then
+        echo "ERROR: Invalid value: BUILD_TYPE=${BUILD_TYPE}"
+        exit 1
+    fi
 
-echo "Static, debug"
-echo " "
-INSTALL_DIRECTORY="/tmp/ocean_install_static_debug"
-echo "Installation directory: ${INSTALL_DIRECTORY}"
-cmake -S "${OCEAN_DIRECTORY}" -B /tmp/ocean_third-party_build_static_debug -DCMAKE_INSTALL_PREFIX="${INSTALL_DIRECTORY}" -DCMAKE_BUILD_TYPE=Debug
-cmake --build /tmp/ocean_third-party_build_static_debug --target install -- -j16
-echo " "
-echo " "
-echo " "
+    LIBRARY_TYPE=$2
+    if [[ ${LIBRARY_TYPE} == "static" ]]; then
+        ENABLE_BUILD_SHARED_LIBS="OFF"
+    elif [[ ${LIBRARY_TYPE} == "shared" ]]; then
+        ENABLE_BUILD_SHARED_LIBS="ON"
+    else
+        echo "ERROR: Invalid value: LIBRARY_TYPE=${LIBRARY_TYPE}"
+        exit 1
+    fi
 
-echo "Shared, release"
-echo " "
-INSTALL_DIRECTORY="/tmp/ocean_install_shared_release"
-echo "Installation directory: ${INSTALL_DIRECTORY}"
-cmake -S "${OCEAN_DIRECTORY}" -B /tmp/ocean_third-party_build_shared_release -DCMAKE_INSTALL_PREFIX="${INSTALL_DIRECTORY}" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-cmake --build /tmp/ocean_third-party_build_shared_release --target install -- -j16
-echo " "
-echo " "
-echo " "
+    OCEAN_THIRD_PARTY_BUILD_DIRECTORY="${OCEAN_THIRD_PARTY_BUILD_ROOT_DIRECTORY}/ocean_third-party_build_${LIBRARY_TYPE}_${BUILD_TYPE}"
+    OCEAN_THIRD_PARTY_INSTALL_DIRECTORY="${OCEAN_THIRD_PARTY_INSTALL_ROOT_DIRECTORY}/ocean_third-party_install_${LIBRARY_TYPE}_${BUILD_TYPE}"
 
-echo "Shared, debug"
-echo " "
-INSTALL_DIRECTORY="/tmp/ocean_install_shared_debug"
-echo "Installation directory: ${INSTALL_DIRECTORY}"
-cmake -S "${OCEAN_DIRECTORY}" -B /tmp/ocean_third-party_build_shared_debug -DCMAKE_INSTALL_PREFIX="${INSTALL_DIRECTORY}" -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON
-cmake --build /tmp/ocean_third-party_build_shared_debug --target install -- -j16
-echo " "
-echo " "
-echo " "
+    echo " "
+    echo "BUILD_TYPE: ${BUILD_TYPE}"
+    echo "LIBRARY_TYPE: ${LIBRARY_TYPE}"
+    echo " "
+    echo "OCEAN_THIRD_PARTY_BUILD_DIRECTORY: ${OCEAN_THIRD_PARTY_BUILD_DIRECTORY}"
+    echo "OCEAN_THIRD_PARTY_INSTALL_DIRECTORY: ${OCEAN_THIRD_PARTY_INSTALL_DIRECTORY}"
+    echo " "
+
+    cmake -S "${OCEAN_THIRD_PARTY_SOURCE_DIRECTORY}" \
+        -B "${OCEAN_THIRD_PARTY_BUILD_DIRECTORY}" \
+        -DCMAKE_INSTALL_PREFIX="${OCEAN_THIRD_PARTY_INSTALL_DIRECTORY}" \
+        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+        -DBUILD_SHARED_LIBS="${ENABLE_BUILD_SHARED_LIBS}"
+
+    cmake --build "${OCEAN_THIRD_PARTY_BUILD_DIRECTORY}" --target install -- -j16
+
+    echo " "
+    echo " "
+    echo " "
+}
+
+run_build Debug static
+run_build Debug shared
+
+run_build Release static
+run_build Release shared
