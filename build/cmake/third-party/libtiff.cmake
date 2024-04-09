133e96a1412c2e8f082e7b433e1c279896e1ac30
Alexander Schneider <aschneider1@meta.com>
2024-04-09 13:36:24 -0700

    third-party component to provide libtiff v4.6.0
    Summary: not used yet in CMakeLists.txt, will be in upcoming diff
    
    Reviewed By: enpe
    
    Differential Revision: D55930456
    
    fbshipit-source-id: 874066936b4e1b09c91288d685af75848a2b7627

---
+++ build/cmake/third-party/libtiff.cmake
@@ -0,0 +1,18 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+message(CHECK_START "libtiff")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+include(FetchContent)
+
+set(tiff-install ON CACHE BOOL "")
+
+FetchContent_Declare(
+  libtiff
+  GIT_REPOSITORY https://github.com/libsdl-org/libtiff.git
+  GIT_TAG        v4.6.0
+)
+
+FetchContent_MakeAvailable(libtiff)
+
+list(POP_BACK CMAKE_MESSAGE_INDENT)
+message(CHECK_PASS "completed")
