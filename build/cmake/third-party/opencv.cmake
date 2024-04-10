35007e962deaabbeefae038bdddc3718c14ec0f7
Alexander Schneider <aschneider1@meta.com>
2024-04-10 12:33:39 -0700

    third-party component for opencv 4.9.0
    Summary: finds our zlib, libpng, libtiff; not connected to CMakeLists.txt yet
    
    Reviewed By: enpe
    
    Differential Revision: D55950304
    
    fbshipit-source-id: d632119b66d7469db7541ca30446682f0a5eaedf

---
+++ build/cmake/third-party/opencv.cmake
@@ -0,0 +1,27 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+message(CHECK_START "opencv")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+include(FetchContent)
+
+cmake_policy(SET CMP0144 NEW)
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
+
+set(ZLIB_ROOT /c/tmp/ocean_third-party_install_release)
+set(PNG_ROOT /c/tmp/ocean_third-party_install_release)
+set(tiff_ROOT /c/tmp/ocean_third-party_install_release)
+
+set(BUILD_ZLIB OFF CACHE BOOL "")
+set(BUILD_PNG OFF CACHE BOOL "")
+set(BUILD_TIFF OFF CACHE BOOL "")
+
+FetchContent_Declare(
+  opencv
+  GIT_REPOSITORY https://github.com/opencv/opencv.git
+  GIT_TAG        4.9.0
+)
+
+FetchContent_MakeAvailable(opencv)
+
+list(POP_BACK CMAKE_MESSAGE_INDENT)
+message(CHECK_PASS "completed")
