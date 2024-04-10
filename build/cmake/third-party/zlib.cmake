7ff35e184e2b4d385ef027f8f14cbccb970f1249
Nils Plath <nilsplath@meta.com>
2024-04-09 17:08:17 -0700

    Remove patch from CMake config of zlib
    Summary: With the previous diff it is no longer necessary to patch this file.
    
    Differential Revision: D55920732
    
    fbshipit-source-id: b9a2109aa200f008a845c3e100abea9de4f0bed7

--- build/cmake/third-party/zlib.cmake
+++ build/cmake/third-party/zlib.cmake
@@ -8,7 +8,6 @@ FetchContent_Declare(
     zlib
     GIT_REPOSITORY https://github.com/madler/zlib.git
     GIT_TAG        51b7f2abdade71cd9bb0e7a373ef2610ec6f9daf # 1.3.1
-    PATCH_COMMAND  ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/zlib/CMakeLists.txt.51b7f2abdade71cd9bb0e7a373ef2610ec6f9daf" "./CMakeLists.txt"
 )
 
 FetchContent_MakeAvailable(zlib)
