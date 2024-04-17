808155c17835dd11f26a7b8d5e825dadd2de1070
Jan Herling <janherling@meta.com>
2024-04-17 14:50:42 -0700

    Added test app for Network for Android
    Summary: Mainly added some glue around the actual test so that we can run it on Android.
    
    Reviewed By: enpe
    
    Differential Revision: D56262010
    
    fbshipit-source-id: 8e0f7fa225b2317d34ad604607d2b8061fc03026

---
+++ impl/application/ocean/test/network/testnetwork/android/TestNetwork.cpp
@@ -0,0 +1,18 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#include "application/ocean/test/network/testnetwork/android/TestNetwork.h"
+
+#include "ocean/platform/android/Utilities.h"
+
+#include "ocean/test/testnetwork/TestNetwork.h"
+
+using namespace Ocean;
+
+void Java_com_meta_ocean_app_test_network_testnetwork_android_TestNetworkActivity_invokeTest(JNIEnv* env, jobject javaThis, jdouble testDuration, jstring testFunctions)
+{
+	ocean_assert(testDuration > 0.0);
+
+	const std::string functions = Platform::Android::Utilities::toAString(env, testFunctions);
+
+	Test::TestNetwork::testNetworkAsynchron(testDuration, functions);
+}
---
+++ impl/application/ocean/test/network/testnetwork/android/TestNetworkActivity.java
@@ -0,0 +1,67 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+package com.meta.ocean.app.test.network.testnetwork.android;
+
+import com.facebook.ocean.base.BaseJni;
+import com.facebook.ocean.platform.android.application.TextActivity;
+
+import android.view.WindowManager;
+
+/**
+ * This class implements the main Activity of the test application of the Network library.
+ * @ingroup applicationtestnetworktestnetworkandroid
+ */
+public class TestNetworkActivity extends TextActivity
+{
+	// load the library
+	static
+	{
+		System.loadLibrary("OceanTestNetwork");
+	}
+
+	/**
+	 * Native interface function to invoke the test.
+	 * @param testDuration Number of seconds for each test, with range (0, infinity)
+	 * @param testFunctions Optional name of test functions to be executed
+	 */
+	private native void invokeTest(double testDuration, String testFunctions);
+
+	/**
+	 * Called when the activity is becoming visible to the user.
+	 */
+	public void onStart()
+	{
+		super.onStart();
+		android.os.Process.setThreadPriority(-20);
+
+		// ensuring that the screenn does not turn off during execution
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+		// Use "" to provide the test result in the text view
+		// Use "STANDARD" to use the LogCat for the test result
+		// Use getExternalFilesDir(null) + "/filename.txt" for a file output
+		final String testOutput = "";
+
+		BaseJni.initialize(testOutput);
+
+		// Define the test duration in seconds.
+		final double testDuration = 2.0;
+
+		// Use "" to execute the entire test
+		// Use e.g. "data" to execute only the Data test
+		// Use e.g. "data, tcpclient" to execute more than one test
+		final String testFunctions = "";
+
+		invokeTest(testDuration, testFunctions);
+	}
+
+	/**
+	 * Called when the activity is no longer visible to the user, because another activity has been resumed and is covering this one.
+	 */
+	public void onStop()
+	{
+		super.onStop();
+
+		BaseJni.exit(0);
+	}
+}
