b457000fbf021b6ea1c094a886f579876489751a
Jan Herling <janherling@meta.com>
2024-04-18 16:17:22 -0700

    Moved PlaneTracker demo app to /metaonly/
    Summary: The plane tracker is located in `metaonly` so we need to move the demo app as well.
    
    Reviewed By: enpe
    
    Differential Revision: D56316703
    
    fbshipit-source-id: 9390aa227cf1fc70a2e04882b77c4b85a70ec928

--- impl/application/ocean/demo/tracking/planetracker/osx/Application.mm
+++
@@ -1,8 +0,0 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-#import <Cocoa/Cocoa.h>
-
-int main(int argc, const char * argv[])
-{
-	return NSApplicationMain(argc, argv);
-}
