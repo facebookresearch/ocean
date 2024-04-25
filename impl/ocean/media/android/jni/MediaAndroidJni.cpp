735ac624208f7baf97176a9da8f4a37a9099a997
Nils Plath <nilsplath@meta.com>
2024-04-24 18:17:09 -0700

    Minor fix
    Summary:
    The register/unregister function must only be run in static builds. When building this library as a dynamic library in CMake, it will cause compiler errors.
    
    Why does this build with Buck, though? Probably because of the generous use of `preferred_linkage = "static",  # workaround to improve dead-code stripping` in all Android targets, which cause them to be linked statically.
    
    Differential Revision: D56546819
    
    fbshipit-source-id: 6e5d4599fd9e9dd7dffe5fd572a1cb2557f33c68

--- impl/ocean/media/android/jni/MediaAndroidJni.cpp
+++ impl/ocean/media/android/jni/MediaAndroidJni.cpp
@@ -6,12 +6,18 @@ using namespace Ocean;
 
 jboolean Java_com_facebook_ocean_media_android_MediaAndroidJni_registerLibrary(JNIEnv* env, jobject javaThis)
 {
+#ifdef OCEAN_RUNTIME_STATIC
 	Media::Android::registerAndroidLibrary();
+#endif
+
 	return true;
 }
 
 jboolean Java_com_facebook_ocean_media_android_MediaAndroidJni_unregisterLibrary(JNIEnv* env, jobject javaThis)
 {
+#ifdef OCEAN_RUNTIME_STATIC
 	Media::Android::unregisterAndroidLibrary();
+#endif
+
 	return true;
 }
