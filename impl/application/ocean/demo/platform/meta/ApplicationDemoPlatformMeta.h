c73962364d438de84aa939527ddc792ecd9f057d
Jan Herling <janherling@meta.com>
2024-04-30 16:30:23 -0700

    Moved OpenXR NativeApplication demo app back to Ocean's public branch
    Summary: Now, as the native application does not use any `/metaonly/` code anymore, we can move it back to public.
    
    Reviewed By: enpe
    
    Differential Revision: D56775962
    
    fbshipit-source-id: e56c2fa93a5e75a27f41dde9e319f42190cd9b5e

---
+++ impl/application/ocean/demo/platform/meta/ApplicationDemoPlatformMeta.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef META_OCEAN_APPLICATION_DEMO_PLATFORM_META_H
+#define META_OCEAN_APPLICATION_DEMO_PLATFORM_META_H
+
+#include "application/ocean/demo/platform/ApplicationDemoPlatform.h"
+
+/**
+ * @ingroup applicationdemoplatform
+ * @defgroup applicationdemoplatformmeta Platform Meta demo applications
+ * @{
+ * All demo applications for Meta platforms.
+ * @}
+ */
+
+#endif // META_OCEAN_APPLICATION_DEMO_PLATFORM_META_H
---
+++ impl/application/ocean/demo/platform/meta/quest/openxr/ApplicationDemoPlatformMetaQuestOpenXR.h
@@ -0,0 +1,16 @@
+// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+#ifndef META_OCEAN_APPLICATION_DEMO_PLATFORM_META_QUEST_OPENXR_H
+#define META_OCEAN_APPLICATION_DEMO_PLATFORM_META_QUEST_OPENXR_H
+
+#include "application/ocean/demo/platform/meta/quest/ApplicationDemoPlatformMetaQuest.h"
+
+/**
+ * @ingroup applicationdemoplatformmetaquest
+ * @defgroup applicationdemoplatformmetaquestopenxr Platform Meta Quest OpenXR demo applications
+ * @{
+ * All demo applications for Meta Quest OpenXR platforms.
+ * @}
+ */
+
+#endif // META_OCEAN_APPLICATION_DEMO_PLATFORM_META_QUEST_OPENXR_H
