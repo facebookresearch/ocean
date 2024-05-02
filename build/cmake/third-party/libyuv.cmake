b86994435b71aacb1c9b605b1e38945cf4dc3126
Alexander Schneider <aschneider1@meta.com>
2024-05-02 15:15:51 -0700

    fix libyuv not finding libjpeg; fix perl-regex error by doing regex in CMake
    Summary: libyuv moved from pass 0 to pass 1
    
    Reviewed By: enpe
    
    Differential Revision: D56898208
    
    fbshipit-source-id: 0f926901377fdfc9bd8057cb1f6eb79003fa368c

--- build/cmake/third-party/libyuv.cmake
+++ build/cmake/third-party/libyuv.cmake
@@ -2,11 +2,15 @@
 message(CHECK_START "libyuv")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
+set(JPEG_ROOT ${CMAKE_INSTALL_PREFIX})
+
+find_package(Git REQUIRED)
+
 CPMAddPackage(
   NAME           libyuv
   GIT_REPOSITORY https://github.com/lemenkov/libyuv.git
   GIT_TAG        ce32eb773fd66239b794abee1e1ee53fdbc15bc0 # master as of 4/29/2024
-  PATCH_COMMAND  ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/libyuv/CMakeLists.txt" .
+  PATCH_COMMAND  ${GIT_EXECUTABLE} apply "${CMAKE_CURRENT_SOURCE_DIR}/libyuv/libyuv.patch"
 )
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
