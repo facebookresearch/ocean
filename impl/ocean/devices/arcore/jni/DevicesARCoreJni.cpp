9dffd50f33a94ca1cf8ed02c6009685d2138b1f1
Nils Plath <nilsplath@meta.com>
2024-05-01 19:08:19 -0700

    Minor fix in DevicesARCoreJni
    Summary: The register/unregister function must only be run in static builds. When building this library as a dynamic library in CMake, it will cause compiler errors.
    
    Differential Revision: D56788964
    
    fbshipit-source-id: 4ae7ae8b9b47d448bfd7d5e6896849a3534c1790

--- impl/ocean/devices/arcore/jni/DevicesARCoreJni.cpp
+++ impl/ocean/devices/arcore/jni/DevicesARCoreJni.cpp
@@ -8,12 +8,18 @@ using namespace Ocean;
 
 jboolean Java_com_facebook_ocean_devices_arcore_DevicesARCoreJni_registerLibrary(JNIEnv* env, jobject javaThis)
 {
+#if defined(OCEAN_RUNTIME_STATIC)
 	Devices::ARCore::registerARCoreLibrary();
+#endif
+
 	return true;
 }
 
 jboolean Java_com_facebook_ocean_devices_arcore_DevicesARCoreJni_unregisterLibrary(JNIEnv* env, jobject javaThis)
 {
+#if defined(OCEAN_RUNTIME_STATIC)
 	Devices::ARCore::unregisterARCoreLibrary();
+#endif
+
 	return true;
 }
