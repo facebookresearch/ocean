b856c2ea054f65f295a0fed7d4ee3175e605f603
Nils Plath <nilsplath@meta.com>
2024-05-02 09:59:17 -0700

    Patch CMake build of libpng
    Summary: Making it so that there are no longer any version numbers in the name of the library and no symbolic links are created.
    
    Reviewed By: ASchneiderMeta
    
    Differential Revision: D56840768
    
    fbshipit-source-id: 370696beebd6bab6866112b65b2bf9a3423e400d

--- build/cmake/third-party/libpng.cmake
+++ build/cmake/third-party/libpng.cmake
@@ -2,6 +2,8 @@
 message(CHECK_START "libpng")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
+find_package(Git REQUIRED)
+
 set(ZLIB_ROOT ${CMAKE_INSTALL_PREFIX})
 
 if(BUILD_SHARED_LIBS)
@@ -19,6 +21,7 @@ CPMAddPackage(
   NAME           libpng
   GIT_REPOSITORY https://github.com/pnggroup/libpng.git
   GIT_TAG        ed217e3e601d8e462f7fd1e04bed43ac42212429 # 1.6.43
+  PATCH_COMMAND  ${GIT_EXECUTABLE} apply "${CMAKE_CURRENT_SOURCE_DIR}/libpng/CMakeLists.txt.patch"
 )
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
