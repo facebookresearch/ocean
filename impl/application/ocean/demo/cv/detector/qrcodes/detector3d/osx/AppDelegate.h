b303208a6f075e60342246af6435abc0db804be8
Jan Herling <janherling@meta.com>
2024-05-03 09:49:11 -0700

    Fixed documentation
    Summary: $title
    
    Reviewed By: enpe
    
    Differential Revision: D56908029
    
    fbshipit-source-id: dcb3012e6d67d0acf7fee59bf1e1fe4d71956433

--- impl/application/ocean/demo/cv/detector/qrcodes/detector3d/osx/AppDelegate.h
+++ impl/application/ocean/demo/cv/detector/qrcodes/detector3d/osx/AppDelegate.h
@@ -4,28 +4,12 @@
 
 /**
  * @ingroup applicationdemocvdetectorqrcode
- * @defgroup applicationdemocvdetectorqrcodeosx FB Messenger Codes (OSX)
+ * @defgroup applicationdemocvdetectorqrcodeosx 3D QR Codes (OSX)
  * @{
- * This application demonstrates the detection and decoding of FB Messenger codes from video frames (e.g., from a live video/webcam).
+ * This application demonstrates the detection and decoding of 3D QR codes from video frames (e.g., from a live video/webcam).
  * The user has the possibility to define the video input source (with preferred frame dimension) via the commando line parameter.
- * This application will run the original implementation of the detection and decoding of the FB Messenger codes and display the decoded payload as well as the runtime performance.
- * This demo application uses the platform independent QRCode class implementing most of the necessary code.
  * This application here is intended for Apple iOS platforms only.
  * @see QRCode, applicationdemocvdetectorqrcode
- *
- * The application can be started with several optional command arguments to configurate the behavior of the application.
- * 1. parameter: Name or filename of the input medium to be used for tracking e.g., "LiveVideoId:0", or "directory/trackingMovie.mp4", or "imageSequence0001.png".<br>
- * 2. parameter: Preferred frame dimenion of the input medium in pixel e.g., "640x480", or "1280x720", or "1920x1080".<br>
- * 3. parameter: Pixel format to be used, e.g., "Y8"<br>
- * 4. parameter: File path of the badge image (BMP), e.g. `/path/to/messenger_code_badge_template.bmp` (required by the original implementation of the FB Messenger code detection) <br>
- * 5. parameter: File path of the bullseye image (BMP), e.g. `/path/to/messenger_code_bullseye_template.bmp` (required by the original implementation of the FB Messenger code detection) <br>
- * 6. parameter: Filepath of the classifier configuration, e.g., `/path/to/MessengerCodeDetectClassifier1.xml` (required by the original implementation of the FB Messenger code detection)<BR>
- * 7. parameter: "loop" - input medium will be looped (if the medium is finite), "noloop" - input medium will be read until it ends (finite medium) or until interrupted, default: "loop". Other values will cause an error.
- *
- * Examples:
- * open democvdetectorqrcode.app
- *
- * open democvdetectorqrcode.app -- args LiveVideoId:0 1280x720
  * @}
  */
 
