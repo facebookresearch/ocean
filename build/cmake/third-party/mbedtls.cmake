c9b9cdcd15f1994728a84af7045e4d94b776e21b
Alexander Schneider <aschneider1@meta.com>
2024-05-03 16:55:10 -0700

    disable AES New Instructions in third-party dep mbedtls
    Summary:
    disabled globally, but really a problem only for ANDROID_ABI=x86
    also enable building mbedtls and curl on all platforms
    
    Reviewed By: enpe
    
    Differential Revision: D56956862
    
    fbshipit-source-id: 3f2c39c2dc6e2a5af3bde003f9c3923ee31bdc68

--- build/cmake/third-party/mbedtls.cmake
+++ build/cmake/third-party/mbedtls.cmake
@@ -5,10 +5,13 @@ list(APPEND CMAKE_MESSAGE_INDENT "  ")
 set(ENABLE_TESTING OFF)
 set(ENABLE_PROGRAMS OFF)
 
+find_package(Git REQUIRED)
+
 CPMAddPackage(
   NAME           mbedtls
   GIT_REPOSITORY https://github.com/Mbed-TLS/mbedtls.git
   GIT_TAG        v3.5.2
+  PATCH_COMMAND  ${GIT_EXECUTABLE} apply "${CMAKE_CURRENT_SOURCE_DIR}/mbedtls/mbedtls.patch"
 )
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
