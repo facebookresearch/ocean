07b5f5800ac1cedcdbd8534f2a6b5503b11de1d2
Nils Plath <nilsplath@meta.com>
2024-05-08 09:37:13 -0700

    Update to OSS license (remaining 3/N)
    Summary:
    Updating the license text for the OSS parts of Ocean.
    
    Details: https://www.internalfb.com/intern/wiki/Open_Source/Licenses/MIT_License/
    
    Differential Revision: D57058642
    
    fbshipit-source-id: 1717c8da8c5e43b98ac2c04846f4ccc40ad2915b

--- impl/ocean/scenedescription/sdx/x3d/BooleanTrigger.h
+++ impl/ocean/scenedescription/sdx/x3d/BooleanTrigger.h
@@ -1,4 +1,7 @@
-// (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// 
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
 
 #ifndef META_OCEAN_SCENEDESCRIPTION_SDX_X3D_BOOLEAN_TRIGGER_H
 #define META_OCEAN_SCENEDESCRIPTION_SDX_X3D_BOOLEAN_TRIGGER_H
