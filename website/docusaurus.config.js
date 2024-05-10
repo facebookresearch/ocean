08595270620a17e150f48146c3654d38b43579eb
Nils Plath <nilsplath@meta.com>
2024-05-10 14:38:51 -0700

    Add a basic Docusaurus website for Ocean
    Summary:
    Added these files as described on
    
    https://www.internalfb.com/intern/staticdocs/staticdocs/docs/setup/3-creating-a-site/
    
    Differential Revision: D57225317
    
    fbshipit-source-id: f7c1ac9cf79b224b9691633732f9367ad51b8c16

---
+++ website/docusaurus.config.js
@@ -0,0 +1,125 @@
+/**
+ * (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+ */
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'Ocean', // TODO
+  tagline: 'Ocean is cool', // TODO
+  url: 'https://internalfb.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  trailingSlash: true,
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook',
+  projectName: 'Ocean', // TODO
+  customFields: {
+    fbRepoName: 'fbsource',
+    ossRepoPath: 'xplat/ocean/website',
+  },
+
+  presets: [
+    [
+      'docusaurus-plugin-internaldocs-fb/docusaurus-preset',
+      /** @type {import('docusaurus-plugin-internaldocs-fb').PresetOptions} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://www.internalfb.com/code/fbsource/xplat/ocean/website', // TODO Please change this to your repo.
+        },
+        experimentalXRepoSnippets: {
+          baseDir: '.',
+        },
+        staticDocsProject: 'Ocean',
+        trackingFile: 'fbcode/staticdocs/WATCHED_FILES',
+        blog: {
+          showReadingTime: true,
+          editUrl: 'https://www.internalfb.com/code/fbsource/xplat/ocean/website', // TODO change to path to your project in fbsource/www
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Ocean',
+        logo: {
+          alt: 'Ocean Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
