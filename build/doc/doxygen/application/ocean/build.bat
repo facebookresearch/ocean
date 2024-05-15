15b061653c22cb760a513c0398094b3356cfbeaa
Nils Plath <nilsplath@meta.com>
2024-05-15 12:44:35 -0700

    Update license texts 4/N
    Summary: As title.
    
    Reviewed By: janherling
    
    Differential Revision: D57389349
    
    fbshipit-source-id: eb81af3aabfd1f07b50945e93876d5f14a1b5274

--- build/doc/doxygen/application/ocean/build.bat
+++ build/doc/doxygen/application/ocean/build.bat
@@ -1,4 +1,7 @@
-@REM (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+@REM Copyright (c) Meta Platforms, Inc. and affiliates.
+@REM
+@REM This source code is licensed under the MIT license found in the
+@REM LICENSE file in the root directory of this source tree.
 
 python3 build.nopy
 application_ocean_doxygen.log
--- build/visual_studio/win/vc143/application/ocean/test/testocean/win/test.bat
+++ build/visual_studio/win/vc143/application/ocean/test/testocean/win/test.bat
@@ -1,2 +1,6 @@
-@REM (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+@REM Copyright (c) Meta Platforms, Inc. and affiliates.
+@REM
+@REM This source code is licensed under the MIT license found in the
+@REM LICENSE file in the root directory of this source tree.
+
 ..\..\..\..\..\..\..\..\..\bin\application\ocean\test\win\win_x64_vc143_static\testocean.exe ..\..\..\..\..\..\..\..\..\data\testsuite\media\images\tracking\sift800x640.bmp ..\..\..\..\..\..\..\..\..\bin\ocean_framework_test.log
