d15677619d51d537c8676292052dc078884cb121
Alexander Schneider <aschneider1@meta.com>
2024-05-15 13:30:26 -0700

    allow commenting out items in dependencies_*.txt with a leading #
    Reviewed By: enpe
    
    Differential Revision: D57397456
    
    fbshipit-source-id: 4fba0a5304a17496a74d7fc712c4b23cf171178e

--- build/cmake/third-party/build_deps.bat
+++ build/cmake/third-party/build_deps.bat
@@ -13,7 +13,7 @@ set INSTALL_DIRECTORY=%3
 set BUILD_TYPE=%4
 set BUILD_SHARED_LIBS=%5
 
-for /F %%d in (%OCEAN_THIRD_PARTY_SOURCE_DIR%\dependencies_allplatforms.txt %OCEAN_THIRD_PARTY_SOURCE_DIR%\dependencies_windows.txt) do (
+for /F "eol=# usebackq delims=" %%d in (%OCEAN_THIRD_PARTY_SOURCE_DIR%\dependencies_allplatforms.txt %OCEAN_THIRD_PARTY_SOURCE_DIR%\dependencies_windows.txt) do (
     echo %%d
     cmake -S %OCEAN_THIRD_PARTY_SOURCE_DIR% -B %BUILD_DIRECTORY_BASE%\%%d -DCMAKE_INSTALL_PREFIX=%INSTALL_DIRECTORY% -DCMAKE_CONFIGURATION_TYPES=%BUILD_TYPE% -DBUILD_SHARED_LIBS=%BUILD_SHARED_LIBS% -DINCLUDED_DEP_NAME=%%d
     cmake --build %BUILD_DIRECTORY_BASE%\%%d --config %BUILD_TYPE% --target install -- /m:16
