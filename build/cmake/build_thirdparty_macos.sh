f4a7ee0d12efba332a9226831bbf66d78b2fb261
Nils Plath <nilsplath@meta.com>
2024-05-15 12:46:31 -0700

    Improve CMake build script for third-party libs on macOS
    Summary:
    Add the following parameters to the build script:
    * `-c` - build configs `debug` or `release`
    * `-b` - build directory
    * `-i` - installation directory
    * `-l` - linkage: `static` or `shared`
    
    Reviewed By: janherling
    
    Differential Revision: D57363843
    
    fbshipit-source-id: a3ed1f6141cb70c3e501ec4e5eec95ba6bbb63ac

--- build/cmake/build_thirdparty_macos.sh
+++ build/cmake/build_thirdparty_macos.sh
@@ -4,72 +4,208 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-
 echo "Building the third-party libraries required for Ocean ...:"
-echo " "
+echo ""
 
-OCEAN_THIRD_PARTY_SOURCE_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../../build/cmake/third-party && pwd )
-OCEAN_THIRD_PARTY_BUILD_ROOT_DIRECTORY="/tmp/ocean/build/macos"
-OCEAN_THIRD_PARTY_INSTALL_ROOT_DIRECTORY="/tmp/ocean/install/macos"
+OCEAN_PLATFORM="macos"
 
-# Builds the third-party libraries for Ocean (Linux & macOS)
-#
-# BUILD_TYPE: The build type to be used, valid values: Debug, Release
-# LIBRARY_TYPE: The type of libraries to be built, valid values: static, shared
-function run_build {
-    BUILD_TYPE=$1
-    if [[ ${BUILD_TYPE} != "Debug" ]] && [[ ${BUILD_TYPE} != "Release" ]]; then
-        echo "ERROR: Invalid value: BUILD_TYPE=${BUILD_TYPE}"
-        exit 1
-    fi
+# OTP = OCEAN_THIRD_PARTY
+OTP_SOURCE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd third-party && pwd )
 
-    LIBRARY_TYPE=$2
-    if [[ ${LIBRARY_TYPE} == "static" ]]; then
-        ENABLE_BUILD_SHARED_LIBS="OFF"
-    elif [[ ${LIBRARY_TYPE} == "shared" ]]; then
-        ENABLE_BUILD_SHARED_LIBS="ON"
-    else
-        echo "ERROR: Invalid value: LIBRARY_TYPE=${LIBRARY_TYPE}"
-        exit 1
-    fi
+OTP_BUILD_DIR="/tmp/ocean/build/${OCEAN_PLATFORM}"
+OTP_INSTALL_DIR="/tmp/ocean/install/${OCEAN_PLATFORM}"
 
-    OCEAN_THIRD_PARTY_BUILD_DIRECTORY="${OCEAN_THIRD_PARTY_BUILD_ROOT_DIRECTORY}/third-party_${LIBRARY_TYPE}_${BUILD_TYPE}"
-    OCEAN_THIRD_PARTY_INSTALL_DIRECTORY="${OCEAN_THIRD_PARTY_INSTALL_ROOT_DIRECTORY}/${LIBRARY_TYPE}_${BUILD_TYPE}"
+OTP_VALID_BUILD_CONFIGS="debug,release"
+OTP_BUILD_CONFIG="${OTP_VALID_BUILD_CONFIGS}"
 
-    echo " "
-    echo "BUILD_TYPE: ${BUILD_TYPE}"
-    echo "LIBRARY_TYPE: ${LIBRARY_TYPE}"
-    echo " "
-    echo "OCEAN_THIRD_PARTY_BUILD_DIRECTORY: ${OCEAN_THIRD_PARTY_BUILD_DIRECTORY}"
-    echo "OCEAN_THIRD_PARTY_INSTALL_DIRECTORY: ${OCEAN_THIRD_PARTY_INSTALL_DIRECTORY}"
-    echo " "
+OTP_VALID_LINKING_TYPES="static,shared"
+OTP_LINKING_TYPES="${OTP_VALID_LINKING_TYPES}"
 
-    echo " "
-    ${OCEAN_THIRD_PARTY_SOURCE_DIRECTORY}/build_deps.sh "${OCEAN_THIRD_PARTY_SOURCE_DIRECTORY}" "${OCEAN_THIRD_PARTY_BUILD_DIRECTORY}" macos "-DCMAKE_INSTALL_PREFIX=${OCEAN_THIRD_PARTY_INSTALL_DIRECTORY}" "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}" "-DBUILD_SHARED_LIBS=${ENABLE_BUILD_SHARED_LIBS}"
-    if [ $? != 0 ]; then
-        failed_configs+=("${LIBRARY_TYPE}_${BUILD_TYPE}_0")
-    fi
+# Collection of builds that have errors that will be listed at the end of the script
+OTP_FAILED_BUILDS=()
 
-    echo " "
-    echo " "
-    echo " "
+# Displays the supported parameters of this script
+display_help()
+{
+    echo "Script to build Ocean:"
+    echo ""
+    echo "  $(basename "$0") [-h|--help] [-i|--install INSTALL_DIR] [-b|--build BUILD_DIR] [-c|--config BUILD_CONFIG]"
+    echo "                   [-l|--link LINKING_TYPE]"
+    echo ""
+    echo "Arguments:"
+    echo ""
+    echo "  -i | -install INSTALL_DIR : The optional location where the third-party libraries of Ocean will"
+    echo "                be installed. Otherwise builds will be installed to: ${OTP_INSTALL_DIR}"
+    echo ""
+    echo "  -b | -build BUILD_DIR : The optional location where the third-party libraries of Ocean will"
+    echo "                be built. Otherwise builds will be installed to: ${OTP_BUILD_DIR}"
+    echo ""
+    echo "  -c | --config BUILD_CONFIG : The optional build configs(s) to be built; valid values are:"
+    for type in $(echo "${OTP_VALID_BUILD_CONFIGS}" | tr ',' '\n'); do
+        echo "                  ${type}"
+    done
+    echo "                Multiple values must be separated by commas. If not specified, both will be built and installed."
+    echo ""
+    echo "  -l | -link LINKING_TYPE : The optional linking type for which will be built; valid values are:"
+    for type in $(echo "${OTP_VALID_LINKING_TYPES}" | tr ',' '\n'); do
+        echo "                  ${type}"
+    done
+    echo "                Multiple values must be separated by commas. If not specified, both will be built and installed."
+    echo ""
+    echo "  -h | --help : This summary"
+    echo ""
 }
 
-run_build Debug static
-run_build Debug shared
+# Builds the third-party libraries for Ocean
+#
+# BUILD_CONFIG: The build type to be used, valid values: Debug, Release
+# LINKING_TYPE: The type of libraries to be built, valid values: static, shared
+function run_build {
+    BUILD_CONFIG=$1
 
-run_build Release static
-run_build Release shared
+    # Convert the name of the build mode to the CMake notation.
+    if [[ ${BUILD_CONFIG} == "debug" ]]; then
+        BUILD_CONFIG="Debug"
+    elif [[ ${BUILD_CONFIG} == "release" ]]; then
+        BUILD_CONFIG="Release"
+    else
+        echo "ERROR: Invalid value: BUILD_CONFIG=${BUILD_CONFIG}" >&2
+        exit 1
+    fi
 
-if [ "${#failed_configs[@]}" -eq 0 ]; then
+    LINKING_TYPE=$2
+    if [[ ${LINKING_TYPE} == "static" ]]; then
+        ENABLE_BUILD_SHARED_LIBS="OFF"
+    elif [[ ${LINKING_TYPE} == "shared" ]]; then
+        ENABLE_BUILD_SHARED_LIBS="ON"
+    else
+        echo "ERROR: Invalid value: LINKING_TYPE=${LINKING_TYPE}" >&2
+        exit 1
+    fi
+
+    OTP_BUILD_DIRECTORY="${OTP_BUILD_DIR}/third-party_${LINKING_TYPE}_${BUILD_CONFIG}"
+    OTP_INSTALL_DIRECTORY="${OTP_INSTALL_DIR}/${LINKING_TYPE}_${BUILD_CONFIG}"
+
+    echo ""
+    echo ""
+    echo ""
+    echo "Build type: ${BUILD_CONFIG}"
+    echo "Linking type: ${LINKING_TYPE}"
+    echo ""
+    echo "Build directory: ${OTP_BUILD_DIRECTORY}"
+    echo "Install directory: ${OTP_INSTALL_DIRECTORY}"
+    echo ""
+    echo ""
+    echo ""
+
+    eval "${OTP_SOURCE_DIR}/build_deps.sh" "${OTP_SOURCE_DIR}" "${OTP_BUILD_DIRECTORY}" ${OCEAN_PLATFORM} "-DCMAKE_INSTALL_PREFIX=${OTP_INSTALL_DIRECTORY}" "-DCMAKE_BUILD_TYPE=${BUILD_CONFIG}" "-DBUILD_SHARED_LIBS=${ENABLE_BUILD_SHARED_LIBS}"
+    if [ $? != 0 ]; then
+        OTP_FAILED_BUILDS+=("${LINKING_TYPE} + ${BUILD_CONFIG}")
+    fi
+
+    echo ""
+    echo ""
+    echo ""
+}
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -h|--help)
+        display_help
+        exit 0
+        ;;
+        -i|--install)
+        OTP_INSTALL_DIR="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -b|--build)
+        OTP_BUILD_DIR="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -c|--config)
+        OTP_BUILD_CONFIG="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -l|--link)
+        OTP_LINKING_TYPES="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)
+        echo "ERROR: Unknown value \"$1\"." >&2
+        exit 1
+        ;;
+    esac
+done
+
+echo ""
+
+if [ "${OTP_BUILD_CONFIG}" == "" ]; then
+    echo "ERROR: At least one build type has to be specified." >&2
+    exit 1
+fi
+
+# Remove duplicate values
+OTP_BUILD_CONFIG=$(echo "$OTP_BUILD_CONFIG" | tr ',' '\n' | sort -u)
+
+# Only allow valid values
+for type in ${OTP_BUILD_CONFIG[@]}; do
+    if ! echo "${OTP_VALID_BUILD_CONFIGS}" | grep -w "$type" > /dev/null; then
+        echo "Error: Unknown build type \"${type}\"" >&2
+        exit 1
+    fi
+done
+
+if [ "${OTP_LINKING_TYPES}" == "" ]; then
+    echo "ERROR: At least one linking type has to be specified." >&2
+    exit 1
+fi
+
+# Remove duplicate values
+OTP_LINKING_TYPES=$(echo "$OTP_LINKING_TYPES" | tr ',' '\n' | sort -u)
+
+# Only allow valid values
+for type in ${OTP_LINKING_TYPES[@]}; do
+    if ! echo "${OTP_VALID_LINKING_TYPES}" | grep -w "$type" > /dev/null; then
+        echo "Error: Unknown build type \"${type}\"" >&2
+        exit 1
+    fi
+done
+
+echo "The third-party libraries will be build for the following combinations:"
+for build_config in ${OTP_BUILD_CONFIG[@]}; do
+    for link_type in ${OTP_LINKING_TYPES[@]}; do
+        echo " * ${build_config} + ${link_type}"
+    done
+done
+
+echo ""
+echo ""
+echo ""
+echo "Install root directory for all builds: ${OTP_INSTALL_DIR}"
+echo ""
+echo ""
+echo ""
+
+for build_config in ${OTP_BUILD_CONFIG[@]}; do
+    for link_type in ${OTP_LINKING_TYPES[@]}; do
+        run_build "${build_config}" "${link_type}"
+    done
+done
+
+if [ "${#OTP_FAILED_BUILDS[@]}" -eq 0 ]; then
     echo "All builds were successful."
 else
     echo "Some builds have failed." >&2
-    for config in "${failed_configs[@]}"; do
+    for config in "${OTP_FAILED_BUILDS[@]}"; do
         echo "- $config" >&2
     done
 fi
 
-if [ "${#failed_configs[@]}" -gt 0 ]; then
+if [ "${#OTP_FAILED_BUILDS[@]}" -gt 0 ]; then
     exit 1
 fi
