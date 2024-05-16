d9693d2de49c1f069116235d19d52ab898632465
Nils Plath <nilsplath@meta.com>
2024-05-15 17:16:49 -0700

    Add attributions to (derived) third-party code used in Ocean
    Summary: Just making sure the attributions are correct.
    
    Reviewed By: janherling
    
    Differential Revision: D57396383
    
    fbshipit-source-id: b31a93a3dbb8e0dda289452ddd9629603dc10e56

--- impl/ocean/math/FourierTransformation.cpp
+++ impl/ocean/math/FourierTransformation.cpp
@@ -1,49 +1,31 @@
-/*M///////////////////////////////////////////////////////////////////////////////////////
-//
-//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
-//
-//  By downloading, copying, installing or using the software you agree to this license.
-//  If you do not agree to this license, do not download, install,
-//  copy or use the software.
-//
-//
-//						Intel License Agreement
-//				For Open Source Computer Vision Library
-//
-// Copyright (C) 2000, Intel Corporation, all rights reserved.
-// Third party copyrights are property of their respective owners.
-//
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-//	* Redistribution's of source code must retain the above copyright notice,
-//	 this list of conditions and the following disclaimer.
-//
-//	* Redistribution's in binary form must reproduce the above copyright notice,
-//	 this list of conditions and the following disclaimer in the documentation
-//	 and/or other materials provided with the distribution.
-//
-//	* The name of Intel Corporation may not be used to endorse or promote products
-//	 derived from this software without specific prior written permission.
-//
-// This software is provided by the copyright holders and contributors "as is" and
-// any express or implied warranties, including, but not limited to, the implied
-// warranties of merchantability and fitness for a particular purpose are disclaimed.
-// In no event shall the Intel Corporation or contributors be liable for any direct,
-// indirect, incidental, special, exemplary, or consequential damages
-// (including, but not limited to, procurement of substitute goods or services;
-// loss of use, data, or profits; or business interruption) however caused
-// and on any theory of liability, whether in contract, strict liability,
-// or tort (including negligence or otherwise) arising in any way out of
-// the use of this software, even if advised of the possibility of such damage.
-//
-//M*/
-
 /*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Portions Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ */
+
+/*
+ * This file contains derived work from
+ *
+ * https://github.com/opencv/opencv/blob/7713c84465b1bbfea112d87ad61312b190a1505d/modules/core/include/opencv2/core/base.hpp
+ * https://github.com/opencv/opencv/blob/7713c84465b1bbfea112d87ad61312b190a1505d/modules/core/include/opencv2/core/types.hpp
+ * https://github.com/opencv/opencv/blob/7713c84465b1bbfea112d87ad61312b190a1505d/modules/core/include/opencv2/core/utility.hpp
+ * https://github.com/opencv/opencv/blob/7713c84465b1bbfea112d87ad61312b190a1505d/modules/core/src/dxt.cpp
+ *
+ * Copyright (C) 2000, Intel Corporation, all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://github.com/opencv/opencv/blob/7713c84465b1bbfea112d87ad61312b190a1505d/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "ocean/base/Frame.h"
@@ -301,6 +283,47 @@ static inline void* alignPtr( const void* ptr, int align = 32 )
 	assert ( (align & (align-1)) == 0 );
 	return (void*)( ((size_t)ptr + align - 1) & ~(size_t)(align-1) );
 }
+
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//						Intel License Agreement
+//				For Open Source Computer Vision Library
+//
+// Copyright (C) 2000, Intel Corporation, all rights reserved.
+// Third party copyrights are property of their respective owners.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//	* Redistribution's of source code must retain the above copyright notice,
+//	 this list of conditions and the following disclaimer.
+//
+//	* Redistribution's in binary form must reproduce the above copyright notice,
+//	 this list of conditions and the following disclaimer in the documentation
+//	 and/or other materials provided with the distribution.
+//
+//	* The name of Intel Corporation may not be used to endorse or promote products
+//	 derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holders and contributors "as is" and
+// any express or implied warranties, including, but not limited to, the implied
+// warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the Intel Corporation or contributors be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including, but not limited to, procurement of substitute goods or services;
+// loss of use, data, or profits; or business interruption) however caused
+// and on any theory of liability, whether in contract, strict liability,
+// or tort (including negligence or otherwise) arising in any way out of
+// the use of this software, even if advised of the possibility of such damage.
+//
+//M*/
 
 /****************************************************************************************\
 								Discrete Fourier Transform
