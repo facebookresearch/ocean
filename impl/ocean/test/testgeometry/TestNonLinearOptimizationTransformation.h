bba64d0165013f09f5eb2910a06a2f2948c9711c
Jan Herling <janherling@meta.com>
2024-05-15 20:50:25 -0700

    Removed deprecated functions in NonLinearOptimizationTransformation
    Summary:
    The `Pinhole` and `FisheyeCamera`-based functions have been replaced by the `AnyCamera`-based function.
    Thus, removing.
    
    Reviewed By: enpe
    
    Differential Revision: D57391026
    
    fbshipit-source-id: 9e52bd8f1827b16c1cad100c68dae133df220cab

--- impl/ocean/test/testgeometry/TestNonLinearOptimizationTransformation.h
+++ impl/ocean/test/testgeometry/TestNonLinearOptimizationTransformation.h
@@ -42,58 +42,6 @@ class OCEAN_TEST_GEOMETRY_EXPORT TestNonLinearOptimizationTransformation
 		static bool test(const double testDuration, Worker* worker);
 
 		/**
-		 * Tests the non linear optimization function for a 6-DOF object transformation with pinhole camera.
-		 * @param testDuration Number of seconds for each test, with range (0, infinity)
-		 * @return True, if succeeded
-		 */
-		static bool testNonLinearOptimizationObjectTransformationPinholeCamera(const double testDuration);
-
-		/**
-		 * Tests the non linear optimizations function for a 6-DOF transformation with pinhole camera, with specified poses and correspondences.
-		 * @param pinholeCamera The pinhole camera model to be used, must be valid
-		 * @param poses The number of camera poses, with range [2, infinity)
-		 * @param correspondences The number of point correspondences, with range [5, infinity)
-		 * @param testDuration Number of seconds for each test, with range (0, infinity)
-		 * @return True, if succeeded
-		 */
-		static bool testNonLinearOptimizationObjectTransformationPinholeCamera(const PinholeCamera& pinholeCamera, const unsigned int poses, const unsigned int correspondences, const double testDuration);
-
-		/**
-		 * Tests the non linear optimization function for a 6-DOF object transformation with fisheye camera.
-		 * @param testDuration Number of seconds for each test, with range (0, infinity)
-		 * @return True, if succeeded
-		 */
-		static bool testNonLinearOptimizationObjectTransformationFisheyeCamera(const double testDuration);
-
-		/**
-		 * Tests the non linear optimizations function for a 6-DOF transformation with fisheye camera, with specified poses and correspondences.
-		 * @param fisheyeCamera The fisheye camera model to be used, must be valid
-		 * @param poses The number of camera poses, with range [2, infinity)
-		 * @param correspondences The number of point correspondences, with range [5, infinity)
-		 * @param testDuration Number of seconds for each test, with range (0, infinity)
-		 * @return True, if succeeded
-		 */
-		static bool testNonLinearOptimizationObjectTransformationFisheyeCamera(const FisheyeCamera& fisheyeCamera, const unsigned int poses, const unsigned int correspondences, const double testDuration);
-
-		/**
-		 * Tests the non linear optimization function for a 6-DOF object transformation with stereo fisheye camera.
-		 * @param testDuration Number of seconds for each test, with range (0, infinity)
-		 * @return True, if succeeded
-		 */
-		static bool testNonLinearOptimizationObjectTransformationStereoFisheyeCamera(const double testDuration);
-
-		/**
-		 * Tests the non linear optimizations function for a 6-DOF transformation with stereo fisheye camera, with specified poses and correspondences.
-		 * @param fisheyeCameraA The first fisheye camera model to be used, must be valid
-		 * @param fisheyeCameraB The second fisheye camera model to be used, must be valid
-		 * @param poses The number of camera poses, with range [2, infinity)
-		 * @param correspondences The number of point correspondences, with range [5, infinity)
-		 * @param testDuration Number of seconds for each test, with range (0, infinity)
-		 * @return True, if succeeded
-		 */
-		static bool testNonLinearOptimizationObjectTransformationStereoFisheyeCamera(const FisheyeCamera& fisheyeCameraA, const FisheyeCamera& fisheyeCameraB, const unsigned int poses, const unsigned int correspondences, const double testDuration);
-
-		/**
 		 * Tests the non linear optimization function for a 6-DOF object transformation with fisheye camera.
 		 * @param testDuration Number of seconds for each test, with range (0, infinity)
 		 * @return True, if succeeded
