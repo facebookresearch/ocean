4608145880937192270fea400e0fdefb7effb2de
Nils Plath <nilsplath@meta.com>
2024-05-29 16:17:40 -0700

    Improve the CMake build script for Linux/Unix
    Summary:
    Added the option to
    * change the build directory
    * change the installation directory
    * (not) specify a third-party library
    * specify link and build types
    
    Reviewed By: janherling
    
    Differential Revision: D57927383
    
    fbshipit-source-id: 04c05a9ed47574a07b130cd178e885759303d1e1

--- build/cmake/build_ocean_linuxunix.sh
+++ build/cmake/build_ocean_linuxunix.sh
@@ -16,56 +16,78 @@ fi
 echo "Building Ocean ...:"
 echo " "
 
-OCEAN_SOURCE_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../.. && pwd )
-OCEAN_BUILD_ROOT_DIRECTORY="/tmp/ocean/build/${OCEAN_PLATFORM}"
-OCEAN_INSTALL_ROOT_DIRECTORY="/tmp/ocean/install/${OCEAN_PLATFORM}"
+OCEAN_SOURCE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../.. && pwd )
+
+OCEAN_BUILD_DIR="/tmp/ocean/build/${OCEAN_PLATFORM}"
+OCEAN_INSTALL_DIR="/tmp/ocean/install/${OCEAN_PLATFORM}"
+
+OCEAN_VALID_BUILD_CONFIGS="debug,release"
+OCEAN_BUILD_CONFIGS="release"
+
+OCEAN_VALID_LINKING_TYPES="static,shared"
+OCEAN_LINKING_TYPES="static"
+
+OCEAN_THIRD_PARTY_DIR=""
+
+# Collection of builds that have errors that will be listed at the end of the script
+OCEAN_FAILED_BUILDS=()
 
 # Displays the supported parameters of this script
 display_help()
 {
     echo "Script to build Ocean (${OCEAN_PLATFORM}):"
     echo ""
-    echo "  $(basename "$0") [-h|--help] [THIRD_PARTY_ROOT_DIRECTORY]"
+    echo "Arguments:"
+    echo ""
+    echo "  $(basename "$0") [-h|--help] [-i|--install INSTALL_DIR] [-b|--build BUILD_DIR] [-c|--config BUILD_CONFIG]"
+    echo "                   [-l|--link LINKING_TYPE] [-t|--third-party OCEAN_THIRD_PARTY_DIR]"
     echo ""
     echo "Arguments:"
     echo ""
-    echo "  THIRD_PARTY_ROOT_DIRECTORY : The optional location where the third-party libraries of Ocean are"
-    echo "                               installed, if they were built manually. Otherwise standard CMake locations"
-    echo "                               will be searched for compatible third-party libraries."
+    echo "  -i | -install INSTALL_DIR : The optional location where the third-party libraries of Ocean will"
+    echo "                be installed. Otherwise builds will be installed to: ${OCEAN_INSTALL_DIR}"
+    echo ""
+    echo "  -b | -build BUILD_DIR : The optional location where the third-party libraries of Ocean will"
+    echo "                be built. Otherwise builds will be installed to: ${OCEAN_BUILD_DIR}"
+    echo ""
+    echo "  -c | --config BUILD_CONFIG : The optional build configs(s) to be built; valid values are:"
+    for type in $(echo "${OCEAN_VALID_BUILD_CONFIGS}" | tr ',' '\n'); do
+        echo "                  ${type}"
+    done
+    echo "                Multiple values must be separated by commas. Default value if nothing is"
+    echo "                specified: \"${OCEAN_VALID_BUILD_CONFIGS}\""
+    echo ""
+    echo "  -l | -link LINKING_TYPE : The optional linking type for which will be built; valid values are:"
+    for type in $(echo "${OCEAN_VALID_LINKING_TYPES}" | tr ',' '\n'); do
+        echo "                  ${type}"
+    done
+    echo "                Multiple values must be separated by commas. Default value if nothing is"
+    echo "                specified: \"${OCEAN_VALID_LINKING_TYPES}\""
+    echo ""
+    echo "  -h | --help : This summary"
+    echo ""
+    echo "  OCEAN_THIRD_PARTY_DIR : The optional location where the third-party libraries of Ocean, if they"
+    echo "                were built manually. Otherwise standard CMake locations will be search for"
+    echo "                compatible third-party libraries."
     echo ""
     echo "  -h | --help                : This summary"
     echo ""
 }
 
-THIRD_PARTY_ROOT_DIRECTORY="${OCEAN_INSTALL_ROOT_DIRECTORY}"
-
-if [[ $# -gt 0 ]]; then
-    key="$1"
-    case $key in
-        -h|--help)
-        display_help
-        exit 0
-        ;;
-        *)
-        THIRD_PARTY_ROOT_DIRECTORY="${key}"
-        shift # past argument
-        ;;
-    esac
-fi
-
-if [ ! -d "${THIRD_PARTY_ROOT_DIRECTORY}" ]; then
-    echo "ERROR: The following directory for the third-party libraries cannot be found: ${THIRD_PARTY_ROOT_DIRECTORY} - did you run the script to build the third-party libraries?"
-    exit 1
-fi
-
 # Builds Ocean (Linux & macOS)
 #
-# BUILD_TYPE: The build type to be used, valid values: Debug, Release
+# BUILD_CONFIG: The build type to be used, valid values: Debug, Release
 # LIBRARY_TYPE: The type of libraries to be built, valid values: static, shared
 function run_build {
-    BUILD_TYPE="$1"
-    if [[ ${BUILD_TYPE} != "Debug" ]] && [[ ${BUILD_TYPE} != "Release" ]]; then
-        echo "ERROR: Invalid value: BUILD_TYPE=${BUILD_TYPE}"
+    BUILD_CONFIG=$1
+
+    # Convert the name of the build mode to the CMake notation.
+    if [[ ${BUILD_CONFIG} == "debug" ]]; then
+        BUILD_CONFIG="Debug"
+    elif [[ ${BUILD_CONFIG} == "release" ]]; then
+        BUILD_CONFIG="Release"
+    else
+        echo "ERROR: Invalid value: BUILD_CONFIG=${BUILD_CONFIG}" >&2
         exit 1
     fi
 
@@ -79,23 +101,23 @@ function run_build {
         exit 1
     fi
 
-    OCEAN_BUILD_DIRECTORY="${OCEAN_BUILD_ROOT_DIRECTORY}/${LIBRARY_TYPE}_${BUILD_TYPE}"
-    OCEAN_INSTALL_DIRECTORY="${OCEAN_INSTALL_ROOT_DIRECTORY}/${LIBRARY_TYPE}_${BUILD_TYPE}"
+    OCEAN_BUILD_DIRECTORY="${OCEAN_BUILD_DIR}/${LIBRARY_TYPE}_${BUILD_CONFIG}"
+    OCEAN_INSTALL_DIRECTORY="${OCEAN_INSTALL_DIR}/${LIBRARY_TYPE}_${BUILD_CONFIG}"
 
-    OCEAN_THIRD_PARTY_DIRECTORY="${THIRD_PARTY_ROOT_DIRECTORY}/${LIBRARY_TYPE}_${BUILD_TYPE}"
+    OCEAN_THIRD_PARTY_DIRECTORY="${OCEAN_THIRD_PARTY_DIR}/${LIBRARY_TYPE}_${BUILD_CONFIG}"
 
     echo " "
-    echo "BUILD_TYPE: ${BUILD_TYPE}"
+    echo "BUILD_CONFIG: ${BUILD_CONFIG}"
     echo "LIBRARY_TYPE: ${LIBRARY_TYPE}"
     echo " "
     echo "OCEAN_BUILD_DIRECTORY: ${OCEAN_BUILD_DIRECTORY}"
     echo "OCEAN_INSTALL_DIRECTORY: ${OCEAN_INSTALL_DIRECTORY}"
 
     CMAKE_CONFIGURE_COMMAND="cmake \\
-    -S \"${OCEAN_SOURCE_DIRECTORY}\" \\
+    -S \"${OCEAN_SOURCE_DIR}\" \\
     -B \"${OCEAN_BUILD_DIRECTORY}\" \\
     -DCMAKE_INSTALL_PREFIX=\"${OCEAN_INSTALL_DIRECTORY}\" \\
-    -DCMAKE_BUILD_TYPE=\"${BUILD_TYPE}\" \\
+    -DCMAKE_BUILD_TYPE=\"${BUILD_CONFIG}\" \\
     -DBUILD_SHARED_LIBS=\"${ENABLE_BUILD_SHARED_LIBS}\""
 
     if [ -n "${OCEAN_THIRD_PARTY_DIRECTORY}" ]; then
@@ -115,7 +137,7 @@ function run_build {
     build_exit_code=$?
 
     if [ "$build_exit_code" -ne 0 ]; then
-        failed_configs+=("${LIBRARY_TYPE}_${BUILD_TYPE}")
+        OCEAN_FAILED_BUILDS+=("${LIBRARY_TYPE}_${BUILD_CONFIG}")
     fi
 
     echo " "
@@ -125,23 +147,127 @@ function run_build {
     return $build_exit_code
 }
 
-failed_configs=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -h|--help)
+        display_help
+        exit 0
+        ;;
+        -i|--install)
+        OCEAN_INSTALL_DIR="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -b|--build)
+        OCEAN_BUILD_DIR="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -c|--config)
+        OCEAN_BUILD_CONFIGS="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -l|--link)
+        OCEAN_LINKING_TYPES="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        -t|--third-party)
+        OCEAN_THIRD_PARTY_DIR="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)
+        echo "ERROR: Unknown value \"$1\"." >&2
+        exit 1
+        ;;
+    esac
+done
 
-run_build Debug static
-run_build Debug shared
+if [ "${OCEAN_THIRD_PARTY_DIR}" == "" ]; then
+    echo ""
+    echo ""
+    echo "WARNING: No location for the Ocean third-party libraries was specified. If this wasn't intentional make sure to build the third-party libraries first. Please refer to the build instructions for details." >&2
+    echo ""
+    echo ""
+else
+    if [ ! -d "${OCEAN_THIRD_PARTY_DIR}" ]; then
+        echo "ERROR: The following directory for the third-party libraries cannot be found: ${OCEAN_THIRD_PARTY_DIR} - did you run the script to build the third-party libraries?" >&2
+        exit 1
+    fi
+fi
 
-run_build Release static
-run_build Release shared
+if [ "${OCEAN_BUILD_CONFIGS}" == "" ]; then
+    echo "ERROR: At least one build type has to be specified." >&2
+    exit 1
+fi
 
-if [ "${#failed_configs[@]}" -eq 0 ]; then
+# Remove duplicate values
+OCEAN_BUILD_CONFIGS=$(echo "$OCEAN_BUILD_CONFIGS" | tr ',' '\n' | sort -u)
+
+# Only allow valid values
+for type in ${OCEAN_BUILD_CONFIGS[@]}; do
+    if ! echo "${OCEAN_VALID_BUILD_CONFIGS}" | grep -w "$type" > /dev/null; then
+        echo "Error: Unknown build type \"${type}\"" >&2
+        exit 1
+    fi
+done
+
+if [ "${OCEAN_LINKING_TYPES}" == "" ]; then
+    echo "ERROR: At least one linking type has to be specified." >&2
+    exit 1
+fi
+
+# Remove duplicate values
+OCEAN_LINKING_TYPES=$(echo "$OCEAN_LINKING_TYPES" | tr ',' '\n' | sort -u)
+
+# Only allow valid values
+for type in ${OCEAN_LINKING_TYPES[@]}; do
+    if ! echo "${OCEAN_VALID_LINKING_TYPES}" | grep -w "$type" > /dev/null; then
+        echo "Error: Unknown build type \"${type}\"" >&2
+        exit 1
+    fi
+done
+
+echo "The third-party libraries will be build for the following combinations:"
+for build_config in ${OCEAN_BUILD_CONFIGS[@]}; do
+    for link_type in ${OCEAN_LINKING_TYPES[@]}; do
+        echo " * ${build_config} + ${link_type}"
+    done
+done
+
+echo ""
+echo ""
+echo ""
+echo "Build directory: ${OCEAN_INSTALL_DIR}"
+echo "Installation directory: ${OCEAN_INSTALL_DIR}"
+echo ""
+echo ""
+echo ""
+
+# Build
+for build_config in ${OCEAN_BUILD_CONFIGS[@]}; do
+    for link_type in ${OCEAN_LINKING_TYPES[@]}; do
+        run_build "${build_config}" "${link_type}"
+    done
+done
+
+# Determine if all of the above builds were successful.
+OCEAN_BUILD_SUCCESSFUL=0
+
+if [ "${#OCEAN_FAILED_BUILDS[@]}" -eq 0 ]; then
+    OCEAN_BUILD_SUCCESSFUL=1
+fi
+
+if [ ${OCEAN_BUILD_SUCCESSFUL} ]; then
     echo "All builds were successful."
 else
     echo "Some builds have failed." >&2
-    for config in "${failed_configs[@]}"; do
+    for config in "${OTP_FAILED_BUILDS[@]}"; do
         echo "- $config" >&2
     done
-fi
 
-if [ "${#failed_configs[@]}" -gt 0 ]; then
     exit 1
 fi
