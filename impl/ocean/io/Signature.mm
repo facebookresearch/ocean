db5e576f3cd4bc66000bd0c9454735c6b28605de
Nils Plath <nilsplath@meta.com>
2024-05-30 14:54:13 -0700

    Fix use of HIDE_STRING_32 in Signature
    Summary: In order to use `HIDE_STRING_32`, it is necessary to use wide strings (`std::wstring`). Otherwise the build fails.
    
    Reviewed By: janherling
    
    Differential Revision: D57933267
    
    fbshipit-source-id: 648ee73d1d58a7b94795f25d8d0cf81c565ea1c0

--- impl/ocean/io/Signature.mm
+++ impl/ocean/io/Signature.mm
@@ -8,12 +8,12 @@
 #include "ocean/io/Signature.h"
 
 #ifdef OCEAN_PLATFORM_BUILD_APPLE_MACOS
-	#include "ocean/base/Binary.h"
-	#include "ocean/base/StringApple.h"
 
-	#include <Foundation/Foundation.h>
-	#include <Security/Security.h>
-#endif
+#include "ocean/base/Binary.h"
+#include "ocean/base/StringApple.h"
+
+#include <Foundation/Foundation.h>
+#include <Security/Security.h>
 
 namespace Ocean
 {
@@ -21,31 +21,29 @@ namespace Ocean
 namespace IO
 {
 
-#ifdef OCEAN_PLATFORM_BUILD_APPLE_MACOS
-
 bool Signature::evaluateCodeSignatureAppleMacos(const std::wstring& filePath, bool& trustedCodeSignature, std::wstring* subjectName)
 {
 	// we must differentiate between a package (.pkg) file and other files
 	// package files are productsigned and other files are codesigned
 
-	const std::wstring::size_type pos = filePath.find_last_of(L".");
+	const std::wstring::size_type dotPos = filePath.find_last_of(L".");
 
-	if (pos != std::wstring::npos && filePath.substr(pos + 1) == L"pkg")
+	if (dotPos != std::wstring::npos && filePath.substr(dotPos + 1) == L"pkg")
 	{
 		// for packages we call the command line tool pkgutil
 
-		std::string commandTool;
-		HIDE_STRING_32("pkgutil --check-signature", commandTool, "\x53\x1E\xDA\xDA\xCA\x04\xAD\x43\x2C\xCC\xD2\xBF\x8B\xAD\x31\xB4\x62\xF9\x5D\x25\x46\xF8\xDB\x34\xB4", "\x42\xD9\x42\x88\x11\x89\x0F\x5F\xCC\xE7\xC4\xF4\x12\x03\x70\xDF\x88\x7E\x68\x0B\x93\xD8\x8E\x46\x39", 25 + 2);
-		ocean_assert(commandTool == "pkgutil --check-signature");
+		std::wstring commandTool;
+		HIDE_STRING_32(L"pkgutil --check-signature", commandTool, L"\x53\x1E\xDA\xDA\xCA\x04\xAD\x43\x2C\xCC\xD2\xBF\x8B\xAD\x31\xB4\x62\xF9\x5D\x25\x46\xF8\xDB\x34\xB4", L"\x42\xD9\x42\x88\x11\x89\x0F\x5F\xCC\xE7\xC4\xF4\x12\x03\x70\xDF\x88\x7E\x68\x0B\x93\xD8\x8E\x46\x39", 25 + 2);
+		ocean_assert(commandTool == L"pkgutil --check-signature");
 
 		NSString* command = [NSString stringWithFormat:@"%@ \"%@\"", StringApple::toNSString(commandTool), StringApple::toNSString(filePath)];
 
 		NSTask* task = [[NSTask alloc] init];
 		NSPipe* pipe = [NSPipe pipe];
 
-		std::string launchPath;
-		HIDE_STRING_32("/bin/bash", launchPath, "\xF6\x59\x5E\xD9\xC3\x80\x26\xB4\x0F", "\x26\x37\xD4\x87\x2F\x8A\x33\x10\xDA", 9 + 2);
-		ocean_assert(launchPath == "/bin/bash");
+		std::wstring launchPath;
+		HIDE_STRING_32(L"/bin/bash", launchPath, L"\xF6\x59\x5E\xD9\xC3\x80\x26\xB4\x0F", L"\x26\x37\xD4\x87\x2F\x8A\x33\x10\xDA", 9 + 2);
+		ocean_assert(launchPath == L"/bin/bash");
 
 		[task setLaunchPath:StringApple::toNSString(launchPath)];
 		[task setArguments:@[ @"-c", command ]];
@@ -61,26 +59,32 @@ bool Signature::evaluateCodeSignatureAppleMacos(const std::wstring& filePath, bo
 			NSData* outputData = [[pipe fileHandleForReading] readDataToEndOfFile];
 
 			if (!outputData)
+			{
 				return false;
+			}
 
 			std::wstring outputString = StringApple::toWString([[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding]);
 
 			// we search in the output string for the "Developer ID Installer"
 			// for productsigned packages the subject identifier will always be "Developer ID Installer"
-			std::string identifier;
-			HIDE_STRING_32("Developer ID Installer: ", identifier, "\x69\x66\x39\xDB\x23\xD0\xAE\xEA\xD0\x29\x39\x5C\x6F\x75\x8D\x92\xEE\xD7\x28\x3A\xF5\x12\x66\x77", "\xB6\xB5\x8A\x8F\xD1\xEA\xB5\x5E\x0A\x6A\xED\x4C\x18\xE4\x68\x1A\xD7\x6C\x19\x96\x87\xEF\x24\x75", 24 + 2);
-			ocean_assert(identifier == "Developer ID Installer: ");
+			std::wstring identifier;
+			HIDE_STRING_32(L"Developer ID Installer: ", identifier, L"\x69\x66\x39\xDB\x23\xD0\xAE\xEA\xD0\x29\x39\x5C\x6F\x75\x8D\x92\xEE\xD7\x28\x3A\xF5\x12\x66\x77", L"\xB6\xB5\x8A\x8F\xD1\xEA\xB5\x5E\x0A\x6A\xED\x4C\x18\xE4\x68\x1A\xD7\x6C\x19\x96\x87\xEF\x24\x75", 24 + 2);
+			ocean_assert(identifier == L"Developer ID Installer: ");
 
-			std::wstring::size_type pos = outputString.find(String::toWString(identifier));
+			std::wstring::size_type pos = outputString.find(identifier);
 
 			if (pos == std::wstring::npos)
+			{
 				return false;
+			}
 
 			outputString = outputString.substr(pos + 24);
 			pos = outputString.find(L"\n");
 
 			if (pos == std::wstring::npos)
+			{
 				return false;
+			}
 
 			*subjectName = outputString.substr(0, pos);
 		}
@@ -137,8 +141,8 @@ bool Signature::evaluateCodeSignatureAppleMacos(const std::wstring& filePath, bo
 	return true;
 }
 
+}
+
+}
+
 #endif //OCEAN_PLATFORM_BUILD_APPLE_MACOS
-
-}
-
-}
