e617da466d9450975d7fe29cdbf97442af4f4049
Jan Herling <janherling@meta.com>
2024-06-07 11:30:40 -0700

    Using default keystore in VRNativeApplication quest app
    Summary: Further, using `.quest` in package name
    
    Reviewed By: enpe
    
    Differential Revision: D58219608
    
    fbshipit-source-id: d2bf52f87fcbb5a63d766b4ff9a51a464a8faea5

--- impl/application/ocean/demo/platform/meta/quest/openxr/vrnativeapplication/quest/OpenXRVRNativeApplicationActivity.java
+++ impl/application/ocean/demo/platform/meta/quest/openxr/vrnativeapplication/quest/OpenXRVRNativeApplicationActivity.java
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.meta.ocean.app.demo.platform.meta.quest.openxr.vrnativeapplication.android;
+package com.meta.ocean.app.demo.platform.meta.quest.openxr.vrnativeapplication.quest;
 
 public class OpenXRVRNativeApplicationActivity extends android.app.NativeActivity
 {
