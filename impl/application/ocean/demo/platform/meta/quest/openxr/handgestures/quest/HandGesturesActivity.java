52ad0bcab9b81a982e12343dec6e1422080772ca
Jan Herling <janherling@meta.com>
2024-06-07 11:30:40 -0700

    Using default keystore in HandGestures quest app
    Summary: Further, using `.quest` in package name.
    
    Reviewed By: enpe
    
    Differential Revision: D58218834
    
    fbshipit-source-id: acb3f8ae69c5837175f86809349cc67527289080

--- impl/application/ocean/demo/platform/meta/quest/openxr/handgestures/quest/HandGesturesActivity.java
+++ impl/application/ocean/demo/platform/meta/quest/openxr/handgestures/quest/HandGesturesActivity.java
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.meta.ocean.app.demo.platform.meta.quest.openxr.handgestures.android;
+package com.meta.ocean.app.demo.platform.meta.quest.openxr.handgestures.quest;
 
 public class HandGesturesActivity extends android.app.NativeActivity
 {
