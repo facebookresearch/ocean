00ba4fefbb4caf33715e233672e3f514e09c707a
Jan Herling <janherling@meta.com>
2024-06-12 11:27:11 -0700

    Added new demo application ExternalCamera for Quest
    Summary:
    This is the first version of the ExternalCamera demo for Quest.
    We still need to improve the demo with some minor details - e.g., better error feedback.
    However, for now the app works as expected.
    
    Reviewed By: enpe
    
    Differential Revision:
    D58443257
    
    Privacy Context Container: L1191897
    
    fbshipit-source-id: ebe7c4c8fbcd9afd5f40876fd94cab4b2b685fdf

---
+++ impl/application/ocean/demo/media/externalcamera/quest/ExternalCameraApplicationMain.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "application/ocean/demo/media/externalcamera/quest/ExternalCameraApplication.h"
+
+#include "ocean/media/usb/USB.h"
+
+using namespace Ocean;
+
+/**
+ * The application's main entry point of native activity of the demo application.
+ *
+ * The demo application shows how to create a native activity using Ocean's rendering engine to render virtual 3D content (a coordinate system in front of the user).
+ * Further, the application is based on Platform::Meta::Quest::OpenXR::VRApplication wrapping most parts parts of the OVR/Android code.
+ *
+ * @param androidApp The android app structure
+ */
+void android_main(struct android_app* androidApp)
+{
+	ocean_assert(androidApp != nullptr);
+
+	Media::USB::registerUSBLibrary();
+
+	ExternalCameraApplication externalCameraApplication(androidApp);
+	externalCameraApplication.run();
+}
