5d7f8fdf6c5bee691cd499811f0859d2a3750668
Alexander Schneider <aschneider1@meta.com>
2024-06-13 16:27:05 -0700

    richer set of command-line flags for build_thirdparty_windows.bat
    Reviewed By: enpe
    
    Differential Revision: D58555624
    
    fbshipit-source-id: 48db5bb64fccbdd8adb7afcb326f2d8d8a757a3f

--- build/cmake/build_thirdparty_windows.bat
+++ build/cmake/build_thirdparty_windows.bat
@@ -10,39 +10,115 @@ setlocal
 @REM Determine the location of the source directory from the location of this script
 set OCEAN_THIRD_PARTY_SOURCE_DIR=%~dp0..\..\build\cmake\third-party
 
+
+@echo off
+setlocal enableDelayedExpansion
+
+set "options=-install:C:\tmp\ocean\install\win -build:C:\tmp\ocean\build\win -config:"debug release" -link:"static shared" -archive:NULL -h:"
+
+for %%O in (%options%) do for /f "tokens=1,* delims=:" %%A in ("%%O") do set "%%A=%%~B"
+:loop
+if not "%~1"=="" (
+  set "test=!options:*%~1:=! "
+  if "!test!"=="!options! " (
+      echo Error: Invalid option %~1
+  ) else if "!test:~0,1!"==" " (
+      set "%~1=1"
+  ) else (
+      setlocal disableDelayedExpansion
+      set "val=%~2"
+      call :escapeVal
+      setlocal enableDelayedExpansion
+      for /f delims^=^ eol^= %%A in ("!val!") do endlocal&endlocal&set "%~1=%%A" !
+      shift /1
+  )
+  shift /1
+  goto :loop
+)
+goto :endArgs
+:escapeVal
+set "val=%val:^=^^%"
+set "val=%val:!=^!%"
+exit /b
+:endArgs
+
+if !-h!==1 (
+    echo Script to build the third-party libraries required by Ocean :
+    echo(
+    echo  %~n0  [-h] [-install INSTALL_DIR] [-build BUILD_DIR] [-config BUILD_CONFIG]
+    echo                    [-link LINKING_TYPE] [-archive ARCHIVE]
+    echo(
+    echo Arguments:
+    echo(
+    echo   -install INSTALL_DIR : The optional location where the third-party libraries of Ocean will
+    echo                 be installed. Otherwise builds will be installed to: !-install!
+    echo(
+    echo   -build BUILD_DIR : The optional location where the third-party libraries of Ocean will
+    echo                 be built. Otherwise builds will be installed to: !-build!
+    echo(
+    echo   -config BUILD_CONFIG : The optional build configs to be built; valid values are:
+    echo                 Multiple values must be separated by spaces (inside double quotes^).
+    echo                 Default value is nothing is specified: !-config!
+    echo(
+    echo   -link LINKING_TYPE : The optional linking type for which will be built; valid values are:
+    echo                 Multiple values must be separated by spaces (inside double quotes^).
+    echo                 Default value if nothing is specified: !-link!
+    echo(
+    echo   -archive ARCHIVE : If specified, this will copy the contents of INSTALL_DIR after the build
+    echo                 into a ZIP archive; the path to this archive must exist.
+    echo(
+    echo   -h : This summary
+    echo(
+    exit /b
+)
+
 set BUILD_FAILURES=
 
-@REM Static, debug
-set BUILD_TYPE=Debug
-set BUILD_SHARED_LIBS=OFF
-set BUILD_DIRECTORY=C:\tmp\ocean\build\win\3P\static_%BUILD_TYPE%
-set INSTALL_DIRECTORY=C:\tmp\ocean\install\win\static_%BUILD_TYPE%
-call :run_build
+for %%c in (!-config!) do (
+  for %%l in (!-link!) do (
+    if /I %%c==debug (
+      set BUILD_TYPE=Debug
+    ) else if /I %%c==release (
+      set BUILD_TYPE=Release
+    ) else (
+      echo Invalid build config %%c
+      exit /b
+    )
 
-@REM Static, release
-set BUILD_TYPE=Release
-set BUILD_SHARED_LIBS=OFF
-set BUILD_DIRECTORY=C:\tmp\ocean\build\win\3P\static_%BUILD_TYPE%
-set INSTALL_DIRECTORY=C:\tmp\ocean\install\win\static_%BUILD_TYPE%
-call :run_build
+    if /I %%l==static (
+      set BUILD_SHARED_LIBS=OFF
+      set bibase=static_!BUILD_TYPE!
+    ) else if /I %%l==shared (
+      set BUILD_SHARED_LIBS=ON
+      set bibase=shared_!BUILD_TYPE!
+    ) else (
+      echo Invalid link mode %%l
+      exit /b
+    )
 
-@REM Shared, debug
-set BUILD_TYPE=Debug
-set BUILD_SHARED_LIBS=ON
-set BUILD_DIRECTORY=C:\tmp\ocean\build\win\3P\shared_%BUILD_TYPE%
-set INSTALL_DIRECTORY=C:\tmp\ocean\install\win\shared_%BUILD_TYPE%
-call :run_build
+    set BUILD_DIRECTORY=!-build!/!bibase!
+    set INSTALL_DIRECTORY=!-install!/!bibase!
 
-@REM Shared, release
-set BUILD_TYPE=Release
-set BUILD_SHARED_LIBS=ON
-set BUILD_DIRECTORY=C:\tmp\ocean\build\win\3P\shared_%BUILD_TYPE%
-set INSTALL_DIRECTORY=C:\tmp\ocean\install\win\shared_%BUILD_TYPE%
-call :run_build
+    echo BUILD_TYPE !BUILD_TYPE!
+    echo BUILD_SHARED_LIBS !BUILD_SHARED_LIBS!
+    echo BUILD_DIRECTORY !BUILD_DIRECTORY!
+    echo INSTALL_DIRECTORY !INSTALL_DIRECTORY!
+
+    call :run_build
+  )
+)
 
 if "%BUILD_FAILURES%" == "" (
     echo All builds were successful.
+
+    if NOT !-archive! == NULL (
+      echo Creating !-archive!
+      pushd !-install! && (tar -a -c -f !-archive! . & popd)
+      echo done
+    )
+
     exit /b 0
+
 ) else (
     echo [91mSome builds have failed.[0m 1>&2
     for %%f in (%BUILD_FAILURES%) do (
@@ -52,11 +128,11 @@ if "%BUILD_FAILURES%" == "" (
 )
 
 :run_build
-call %OCEAN_THIRD_PARTY_SOURCE_DIR%\build_deps.bat windows %OCEAN_THIRD_PARTY_SOURCE_DIR% %BUILD_DIRECTORY% /m:16 "-DCMAKE_INSTALL_PREFIX=%INSTALL_DIRECTORY%" "-DCMAKE_CONFIGURATION_TYPES=%BUILD_TYPE%" "-DBUILD_SHARED_LIBS=%BUILD_SHARED_LIBS%"
+call %OCEAN_THIRD_PARTY_SOURCE_DIR%\build_deps.bat windows %OCEAN_THIRD_PARTY_SOURCE_DIR% !BUILD_DIRECTORY! /m:16 "-DCMAKE_INSTALL_PREFIX=!INSTALL_DIRECTORY!" "-DCMAKE_CONFIGURATION_TYPES=!BUILD_TYPE!" "-DBUILD_SHARED_LIBS=!BUILD_SHARED_LIBS!"
 
 @echo off
 if %errorlevel% neq 0 (
-    set BUILD_FAILURES=%BUILD_FAILURES% %LIBRARY_TYPE%_%BUILD_TYPE%_pass%BUILD_PASS%
+    set BUILD_FAILURES=%BUILD_FAILURES% !BUILD_SHARED_LIBS!_%BUILD_TYPE%_pass%BUILD_PASS%
     exit /b 1
 ) else (
     exit /b 0
--- building_for_windows.md
+++ building_for_windows.md
@@ -13,27 +13,27 @@ This document describes the process to build Ocean for Windows. It covers:
 
 ## 2 Building the third-party libraries
 
-The easiest way to build the third-party libraries is by using the provided build script, [`build/cmake/build_thirdparty_windows.bat`](build/cmake/build_thirdparty_windows.bat). By default, this will build all third-party libraries in both debug and release configurations. Each of these will be built with static as well as dynamic linking. To build a specific configuration, use the parameters `--config BUILD_TYPE` and `--link LINKING_TYPE`. For example to build only the shared debug configurations, run:
+The easiest way to build the third-party libraries is by using the provided build script, [`build/cmake/build_thirdparty_windows.bat`](build/cmake/build_thirdparty_windows.bat). By default, this will build all third-party libraries in both debug and release configurations. Each of these will be built with static as well as dynamic linking. To build a specific configuration, use the parameters `-config BUILD_TYPE` and `-link LINKING_TYPE`. For example to build only the shared debug configurations, run:
 
 ```
-./build/cmake/build_thirdparty_windows.bat --config debug --link shared
+./build/cmake/build_thirdparty_windows.bat -config debug -link shared
 ```
 
 The default installation location will be shown in the logs of the build script and similar to `C:/tmp/ocean/install/win`. A custom location for the installation can specified as follows:
 
 ```
-./build/cmake/build_thirdparty_windows.bat --install /path/to/install/root/dir
+./build/cmake/build_thirdparty_windows.bat -install /path/to/install/root/dir
 ```
 
 Methods for specifying the installation directory, in descending order of preference are:
-1. Using `--install /path/to/install/root/dir`
+1. Using `-install /path/to/install/root/dir`
 2. Using the environment variable `OCEAN_INSTALL_PATH`
 3. Change the hard-coded default in the build script, `C:/tmp/ocean/install`
 
 Similarly, the root directory where the code should be built can be specified as follows:
 
 ```
-./build/cmake/build_thirdparty_windows.bat --build /path/to/build/root
+./build/cmake/build_thirdparty_windows.bat -build /path/to/build/root
 ```
 
 Otherwise the code will be built in `C:/tmp/ocean/build/win`.
