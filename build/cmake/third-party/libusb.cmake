002de10872889b5bb0076293dae6a7aaa5fbb5a4
Alexander Schneider <aschneider1@meta.com>
2024-06-27 14:00:39 -0700

    libusb.h lands in include, not in include/libusb-1.0/
    Summary: Also change libusb-1.0 in library names to libusb
    
    Reviewed By: enpe
    
    Differential Revision: D59123378
    
    fbshipit-source-id: 520ce36723e257cdee9c9baf06ecd0d8493ac17e

--- build/cmake/third-party/libusb.cmake
+++ build/cmake/third-party/libusb.cmake
@@ -6,12 +6,15 @@
 message(CHECK_START "libusb")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
+find_package(Git REQUIRED)
+
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 CPMAddPackage(
   NAME           libusb
   GIT_REPOSITORY https://github.com/libusb/libusb-cmake.git
   GIT_TAG        cec1e49eec481a6333c218726ecaba8e4f447a07
+  PATCH_COMMAND  ${GIT_EXECUTABLE} apply --ignore-whitespace "${CMAKE_CURRENT_SOURCE_DIR}/libusb/libusb.patch"
 )
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
