545bf3591baca24904f01c35b0b51757c37b851b
Alexander Schneider <aschneider1@meta.com>
2024-11-01 17:07:09 -0700

    update wxwidgets to 3.2.6 and patch to support building on OSX 15
    Summary:
    CGDisplayCreateImage is no longer available. This diff is a port of:
    https://github.com/wxWidgets/wxWidgets/pull/24879/commits/5b55f6b4aabbd84af23065cc5d930b66282ba496
    
    Reviewed By: enpe
    
    Differential Revision:
    D65296120
    
    Privacy Context Container: L1191897
    
    fbshipit-source-id: c2c526c389b11d2014fe710a7b07ca6d8cbd41e9

--- build/cmake/third-party/wxwidgets.cmake
+++ build/cmake/third-party/wxwidgets.cmake
@@ -6,6 +6,8 @@
 message(CHECK_START "wxwidgets")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
+find_package(Git REQUIRED)
+
 if(BUILD_SHARED_LIBS)
   set(wxBUILD_SHARED ON CACHE BOOL "")
   set(wxWidgets_USE_STATIC OFF)
@@ -22,7 +24,8 @@ set(CMAKE_DEBUG_POSTFIX "")
 CPMAddPackage(
   NAME           wxwidgets
   GIT_REPOSITORY https://github.com/wxWidgets/wxWidgets.git
-  GIT_TAG        v3.2.4
+  GIT_TAG        v3.2.6
+  PATCH_COMMAND  ${GIT_EXECUTABLE} apply --ignore-whitespace "${CMAKE_CURRENT_SOURCE_DIR}/wxwidgets/wxwidgets.patch"
 )
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
