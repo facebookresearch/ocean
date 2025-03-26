9f07dfc83def52f53afba867f3b1b13a869ea73a
Jan Herling <janherling@meta.com>
2025-03-26 12:27:17 -0700

    Fixed documentation
    Summary: $title
    
    Reviewed By: enpe
    
    Differential Revision:
    D71847177
    
    Privacy Context Container: L1293349
    
    fbshipit-source-id: fe391742c690e04e2d404726ca8918e38ce9b02f

--- impl/application/ocean/demo/media/externalcamera/quest/ExternalCameraApplicationMain.cpp
+++ impl/application/ocean/demo/media/externalcamera/quest/ExternalCameraApplicationMain.cpp
@@ -12,7 +12,7 @@ using namespace Ocean;
 /**
  * The application's main entry point of native activity of the demo application.
  *
- * The demo application shows how to create a native activity using Ocean's rendering engine to render virtual 3D content (a coordinate system in front of the user).
+ * The demo application shows how to access an external USB camera.
  * Further, the application is based on Platform::Meta::Quest::OpenXR::VRApplication wrapping most parts parts of the OVR/Android code.
  *
  * @param androidApp The android app structure
