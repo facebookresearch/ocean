98c8b33e6463e39ae88f23ccdd43dd01c2dacb47
Jan Herling <janherling@meta.com>
2025-11-24 22:29:27 -0800

    Added possibility to configure the unit test executed on Android phones when starting
    Summary: $title
    
    Reviewed By: enpe
    
    Differential Revision:
    D87834946
    
    Privacy Context Container: L1192943
    
    fbshipit-source-id: 1868162f3661d4c8342bf3d03d5d27538f25956c

--- impl/application/ocean/test/media/testmedia/android/TestMediaActivity.java
+++ impl/application/ocean/test/media/testmedia/android/TestMediaActivity.java
@@ -10,10 +10,17 @@ package com.meta.ocean.app.test.media.testmedia.android;
 import com.meta.ocean.base.BaseJni;
 import com.meta.ocean.platform.android.application.TextActivity;
 
+import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
 /**
  * This class implements the main Activity of the test application of the Media library.
+ * By default, the app will run all tests for 2.0 seconds each.<br>
+ * In addition, the user can configure the test execution through an intent, the app can be started and configured via:<br>
+ * <pre>
+ * adb shell am start -n com.meta.ocean.app.test.media.testmedia.android/.TestMediaActivity --es test_functions '"special, android.videoencoderdecoder"' --ed test_duration 5.0
+ * </pre>
  * @ingroup applicationtestmediatestmediaandroid
  */
 public class TestMediaActivity extends TextActivity
@@ -30,6 +37,54 @@ public class TestMediaActivity extends TextActivity
 	 * @param testFunctions Optional name of test functions to be executed
 	 */
 	private native void invokeTest(double testDuration, String testFunctions);
+
+	/**
+	 * Reads intent extras for test configuration.
+	 */
+	private void readIntentExtras()
+	{
+		Log.d("Ocean", "TestMediaActivity: readIntentExtras() called");
+
+		if (getIntent() != null)
+		{
+			Log.d("Ocean", "TestMediaActivity: Intent is not null");
+			Bundle extras = getIntent().getExtras();
+
+			if (extras != null)
+			{
+				Log.d("Ocean", "TestMediaActivity: Extras bundle is not null");
+				Log.d("Ocean", "TestMediaActivity: Extras contents: " + extras.toString());
+
+				if (extras.containsKey(EXTRA_TEST_FUNCTIONS))
+				{
+					configuredTestFunctions_ = extras.getString(EXTRA_TEST_FUNCTIONS);
+					Log.d("Ocean", "TestMediaActivity: Found test_functions: " + configuredTestFunctions_);
+				}
+				else
+				{
+					Log.d("Ocean", "TestMediaActivity: No test_functions in extras");
+				}
+
+				if (extras.containsKey(EXTRA_TEST_DURATION))
+				{
+					configuredTestDuration_ = extras.getDouble(EXTRA_TEST_DURATION);
+					Log.d("Ocean", "TestMediaActivity: Found test_duration: " + configuredTestDuration_);
+				}
+				else
+				{
+					Log.d("Ocean", "TestMediaActivity: No test_duration in extras");
+				}
+			}
+			else
+			{
+				Log.d("Ocean", "TestMediaActivity: Extras bundle is null");
+			}
+		}
+		else
+		{
+			Log.d("Ocean", "TestMediaActivity: Intent is null");
+		}
+	}
 
 	/**
 	 * Called when the activity is becoming visible to the user.
@@ -50,13 +105,29 @@ public class TestMediaActivity extends TextActivity
 
 		BaseJni.initialize(applicationOutput);
 
-		// Define the test duration in seconds.
-		final double testDuration = 2.0;
+		// Read intent extras to check for configuration
+		readIntentExtras();
 
+		// Define the test duration in seconds.
 		// Use "" to execute the entire test
-		// Use e.g. "line3" to execute only the Line3 test
-		// Use e.g. "line2, squarematrix3" to execute more than one test
-		final String testFunctions = "";
+
+		// Use e.g. "android.videoencoderdecoder" to execute only the Android.VideoEncoderDecoder test
+		double testDuration = 2.0;
+		String testFunctions = "";
+
+		if (configuredTestDuration_ != null)
+		{
+			Log.d("Ocean", "TestMediaActivity: Using configured test duration: " + configuredTestDuration_);
+			testDuration = configuredTestDuration_;
+		}
+
+		if (configuredTestFunctions_ != null)
+		{
+			Log.d("Ocean", "TestMediaActivity: Using configured test functions: " + configuredTestFunctions_);
+			testFunctions = configuredTestFunctions_;
+		}
+
+		Log.d("Ocean", "TestMediaActivity: Starting tests with duration=" + testDuration + ", functions='" + testFunctions + "'");
 
 		invokeTest(testDuration, testFunctions);
 	}
@@ -70,4 +141,12 @@ public class TestMediaActivity extends TextActivity
 
 		BaseJni.exit(0);
 	}
+
+	// Intent extra keys
+	public static final String EXTRA_TEST_FUNCTIONS = "test_functions";
+	public static final String EXTRA_TEST_DURATION = "test_duration";
+
+	// Member variables for configured values from intent
+	private String configuredTestFunctions_ = null;
+	private Double configuredTestDuration_ = null;
 }
