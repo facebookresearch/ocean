cce93777b1fe9c89b64dd9d11f4be320edffdd79
Jan Herling <janherling@meta.com>
2026-01-21 14:36:30 -0800

    Added Visual Studio 2026 project files
    Summary:
    ## Context
    
    The high-level intention of this stack of diffs is to improve the maintainability and consistency of the build configuration for the Ocean project. The diffs aim to reduce duplication and simplify future updates by centralizing configuration folder paths and compiler version variables.
    
    Specifically, the diffs:
    
    1. Define configuration folder names as computed MSBuild properties in a common props file (ocean_common.props) to avoid repetition in child props files.
    2. Update application and test build property sheets to use the centralized configuration folder variable, eliminating ~1000 lines of duplicated code.
    3. Configure the debugger PATH for shared builds in application props to ensure the correct DLL path is set when debugging shared configurations.
    
    Overall, the purpose of this stack of diffs is to make the build configuration more maintainable, consistent, and efficient, allowing for easier future updates and reducing the risk of errors.
    
    ## This Diff
    
    Here is a summary of the diff in markdown format:
    
    ### Added Visual Studio 2026 project files
    
    This diff adds new Visual Studio project files for various Ocean projects. The added files include:
    
    * `demomathcameraundistortion_reference.props`: a project reference file for the demo math camera undistortion project
    * `demotrackinghomographyimagealigner_reference.props`: a project reference file for the demo tracking homography image aligner project
    * `trackingoffline.vcxproj.filters`: a filters file for the tracking offline project
    * `ocean_common.props`: a common properties file for Ocean projects
    * `testcvsegmentation_reference.props`: a project reference file for the test cv segmentation project
    
    These files were auto-generated by the `generate_reference_props.ps1` script and should not be edited manually. Instead, the script should be re-run to regenerate the files as needed.
    
    Reviewed By: enpe
    
    Differential Revision: D91059672
    
    fbshipit-source-id: c8311e407623022af5dc1be77299b428f2a65199

---
+++ build/visual_studio/win/vc145/build_win_vc145.bat
@@ -0,0 +1,102 @@
+@REM Copyright (c) Meta Platforms, Inc. and affiliates.
+@REM
+@REM This source code is licensed under the MIT license found in the
+@REM LICENSE file in the root directory of this source tree.
+
+@echo off
+
+echo.
+echo Start building WIN vc145...
+echo.
+echo.
+echo.
+
+REM Dynamically locate Visual Studio 2022+ installation using vswhere.exe
+REM -version "[17.0,)" requires VS 2022 (v17.x) or higher
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version "[17.0,)" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do set VSINSTALLPATH=%%i
+
+if not defined VSINSTALLPATH (
+    echo ERROR: Could not find Visual Studio 2022 or higher. Please ensure Visual Studio 2022+ is installed.
+    exit /b 1
+)
+
+set PATH=%PATH%;%VSINSTALLPATH%\MSBuild\Current\Bin
+
+echo Using Visual Studio from: %VSINSTALLPATH%
+echo.
+
+echo 1 / 12 Build Shared Debug x86...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Shared Debug" /p:platform="Win32"
+echo.
+echo.
+
+echo 2 / 12 Build Shared Profile x86...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Shared Profile" /p:platform="Win32"
+echo.
+echo.
+
+echo 3 / 12 Build Shared Release x86...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Shared Release" /p:platform="Win32"
+echo.
+echo.
+
+echo 4 / 12 Build Static Debug x86...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Static Debug" /p:platform="Win32"
+echo.
+echo.
+
+echo 5 / 12 Build Static Profile x86...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Static Profile" /p:platform="Win32"
+echo.
+echo.
+
+echo 6 / 12 Build Static Release x86...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Static Release" /p:platform="Win32"
+echo.
+echo.
+
+echo 7 / 12 Build Shared Debug x64...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Shared Debug" /p:platform="x64"
+echo.
+echo.
+
+echo 8 / 12 Build Shared Profile x64...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Shared Profile" /p:platform="x64"
+echo.
+echo.
+
+echo 9 / 12 Build Shared Release x64...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Shared Release" /p:platform="x64"
+echo.
+echo.
+
+echo 10 / 12 Build Static Debug x64...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Static Debug" /p:platform="x64"
+echo.
+echo.
+
+echo 11 / 12 Build Static Profile x64...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Static Profile" /p:platform="x64"
+echo.
+echo.
+
+echo 12 / 12 Build Static Release x64...
+echo.
+msbuild.exe "win_vc145.sln" /t:build /m:8 /v:m /clp:WarningsOnly /clp:ErrorsOnly /clp:Summary /p:configuration="Static Release" /p:platform="x64"
+echo.
+echo.
+
+if not "%1" == "nopause" (
+pause
+)
