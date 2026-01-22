3b82f05e93cdf0a85e32445dd3b2937a2f52372a
Jan Herling <janherling@meta.com>
2026-01-21 20:59:57 -0800

    Updated the CameraCalibrationManager demo app
    Summary: Now, the app is not using the `LegacyCameraCalibrationManager` anymore but the new `CameraCalibrationManager` instead.
    
    Reviewed By: enpe
    
    Differential Revision: D91184622
    
    fbshipit-source-id: ab71dbc529c437d657d83b27bcd1ede5abca8c6e

--- impl/application/ocean/demo/io/cameracalibrationmanager/CameraCalibrationManager.cpp
+++ impl/application/ocean/demo/io/cameracalibrationmanager/CameraCalibrationManager.cpp
@@ -7,69 +7,194 @@
 
 #include "application/ocean/demo/io/cameracalibrationmanager/CameraCalibrationManager.h"
 
-#include "ocean/io/LegacyCameraCalibrationManager.h"
+#include "ocean/base/CommandArguments.h"
+
+#include "ocean/io/CameraCalibrationManager.h"
+#include "ocean/io/Directory.h"
+#include "ocean/io/File.h"
 
 #include "ocean/math/Numeric.h"
 
-#include "ocean/platform/win/System.h"
+#include "ocean/platform/Utilities.h"
+
+#ifdef _WINDOWS
+	#include "ocean/platform/win/System.h"
+#endif
 
 using namespace Ocean;
-using namespace Ocean::IO;
 
 #if defined(_WINDOWS)
 	// main function on Windows platforms
-	int wmain(int /*argc*/, wchar_t** /*argv*/)
+	int wmain(int argc, wchar_t* argv[])
 #elif defined(__APPLE__)
 	// main function on OSX platforms
-	int main(int /*argc*/, char** /*argv*/)
+	int main(int argc, char* argv[])
 #else
 	#error Missing implementation.
 #endif
 {
+	Messenger::get().setOutputType(Messenger::MessageOutput(Messenger::OUTPUT_STANDARD | Messenger::OUTPUT_DEBUG_WINDOW));
+
+	CommandArguments commandArguments("Demo application for camera calibration manager");
+	commandArguments.registerNamelessParameters("Optional the first command argument is interpreted as calibration file");
+	commandArguments.registerParameter("help", "h", "Showing this help output.");
+	commandArguments.registerParameter("calibration", "c", "The filename of the camera calibration file (*.occ or *.json)");
+	commandArguments.registerParameter("serial", "s", "Optional: the device serial number for device-specific calibrations");
+	commandArguments.registerParameter("version", "v", "Optional: the device hardware version for device-specific calibrations");
+	commandArguments.registerParameter("product", "p", "Optional: the device product name for device-specific calibrations");
+
+	commandArguments.parse(argv, size_t(argc));
+
+	if (commandArguments.hasValue("help"))
+	{
+		Log::info() << commandArguments.makeSummary();
+		return 0;
+	}
+
+#ifdef _WINDOWS
 	const std::string frameworkPath(Platform::Win::System::environmentVariable("OCEAN_DEVELOPMENT_PATH"));
-	LegacyCameraCalibrationManager::get().registerCalibrationFile(frameworkPath + std::string("/data/cameracalibration/cameracalibration.occ"));
+#else
+	const std::string frameworkPath(Platform::System::environmentVariable("OCEAN_DEVELOPMENT_PATH"));
+#endif
 
-	const std::string cameraNames[] =
+	IO::File cameraCalibrationFile;
+
+	Value calibrationValue;
+	if (commandArguments.hasValue("calibration", &calibrationValue, false, 0u) && calibrationValue.isString())
 	{
-		"Logitech QuickCam for Notebooks Pro",
-		"Logitech QuickCam for Notebooks Pro",
-		"Logitech HD Pro Webcam C920",
-		"Logitech HD Pro Webcam C920",
-		"Unknown Camera"
-	};
+		const IO::File file(calibrationValue.stringValue());
 
-	const Indices32 cameraWidths = {320u, 640u, 800u, 1920u};
-	const Indices32 cameraHeights = {240u, 480u, 600u, 1080u};
-
-	ocean_assert(cameraWidths.size() == cameraHeights.size());
-
-	for (size_t n = 0; n < cameraWidths.size(); ++n)
-	{
-		std::cout << "Calibration \"" << cameraNames[n] << "\" with resolution " << cameraWidths[n] << "x" << cameraHeights[n] << ":" << std::endl;
-
-		LegacyCameraCalibrationManager::Quality calibrationQuality = LegacyCameraCalibrationManager::QUALITY_DEFAULT;
-		const PinholeCamera pinholeCamera = LegacyCameraCalibrationManager::get().camera(cameraNames[n], cameraWidths[n], cameraHeights[n], &calibrationQuality);
-
-		std::cout << "Horizontal field of view: " << String::toAString(Numeric::rad2deg(pinholeCamera.fovX()), 1u) << "deg, vertical field of view " << String::toAString(Numeric::rad2deg(pinholeCamera.fovY()), 1u) << "deg." << std::endl;
-
-		if (calibrationQuality == LegacyCameraCalibrationManager::QUALITY_EXACT)
+		if (file.exists())
 		{
-			std::cout << "Quality: Exact." << std::endl << std::endl;
+			cameraCalibrationFile = file;
 		}
-
-		if (calibrationQuality == LegacyCameraCalibrationManager::QUALITY_INTERPOLATED)
+		else
 		{
-			std::cout << "Quality: Interpolated." << std::endl << std::endl;
-		}
-
-		if (calibrationQuality == LegacyCameraCalibrationManager::QUALITY_DEFAULT)
-		{
-			std::cout << "Quality: Default." << std::endl << std::endl;
+			Log::warning() << "Warning: Specified calibration file '" << calibrationValue.stringValue() << "' does not exist.";
 		}
 	}
 
-	std::cout << "Press a key to exit" << std::endl;
-	getchar();
+	if (cameraCalibrationFile.isNull())
+	{
+		const IO::File relativeFile("res/ocean/cv/calibration/camera_calibration.json");
+
+		cameraCalibrationFile = IO::Directory(frameworkPath) + relativeFile;
+	}
+
+	if (cameraCalibrationFile.exists())
+	{
+		Log::info() << "Using calibration file: " << cameraCalibrationFile();
+		IO::CameraCalibrationManager::get().registerCalibrations(cameraCalibrationFile());
+	}
+	else
+	{
+		Log::error() << "Error: No valid calibration file found.";
+		return 1;
+	}
+
+	std::string serialValue;
+	if (commandArguments.hasValue("serial", serialValue))
+	{
+		if (IO::CameraCalibrationManager::get().setDeviceSerial(serialValue))
+		{
+			Log::info() << "Device context set to serial: " << serialValue;
+		}
+		else
+		{
+			Log::warning() << "Failed to set device serial: " << serialValue;
+		}
+	}
+	else
+	{
+		std::string versionValue;
+		if (commandArguments.hasValue("version", versionValue))
+		{
+			if (IO::CameraCalibrationManager::get().setDeviceVersion(versionValue))
+			{
+				Log::info() << "Device context set to version: " << versionValue;
+			}
+			else
+			{
+				Log::warning() << "Failed to set device version: " << versionValue;
+			}
+		}
+		else
+		{
+			std::string productValue;
+			if (commandArguments.hasValue("product", productValue))
+			{
+				if (IO::CameraCalibrationManager::get().setDeviceProduct(productValue))
+				{
+					Log::info() << "Device context set to product: " << productValue;
+				}
+				else
+				{
+					Log::warning() << "Failed to set device product: " << productValue;
+				}
+			}
+		}
+	}
+
+	const IndexPairs32 cameraResolutions =
+	{
+		{320u, 240u},
+		{640u, 480u},
+		{800u, 600u},
+		{1280u, 720u},
+		{1920u, 1080u}
+	};
+
+	const Strings cameraNames = IO::CameraCalibrationManager::get().cameraNames();
+
+	Log::info() << "Available cameras: " << cameraNames.size();
+
+	for (const std::string& cameraName : cameraNames)
+	{
+		if (cameraName != cameraNames.front())
+		{
+			Log::info() << " ";
+		}
+
+		Log::info() << cameraName << ":";
+
+		for (const IndexPair32& cameraResolution : cameraResolutions)
+		{
+			Log::info() << "  " << cameraResolution.first << "x" << cameraResolution.second;
+
+			IO::CameraCalibrationManager::CalibrationQuality calibrationQuality = IO::CameraCalibrationManager::CQ_UNKNOWN;
+			SharedAnyCamera camera = IO::CameraCalibrationManager::get().camera(cameraName, cameraResolution.first, cameraResolution.second, &calibrationQuality);
+
+			if (camera)
+			{
+				Log::info() << "Camera name: " << camera->name();
+
+				std::string qualityString;
+
+				switch (calibrationQuality)
+				{
+					case IO::CameraCalibrationManager::CQ_UNKNOWN:
+						qualityString = "unknown";
+						break;
+
+					case IO::CameraCalibrationManager::CQ_INTERPOLATED:
+						qualityString = "interpolated";
+						break;
+
+					case IO::CameraCalibrationManager::CQ_EXACT:
+						qualityString = "exact";
+						break;
+				}
+
+				ocean_assert(!qualityString.empty());
+
+				Log::info() << "Calibration quality: " << qualityString;
+			}
+			else
+			{
+				Log::info() << "No calibration available";
+			}
+		}
+	}
 
 	return 0;
 }
