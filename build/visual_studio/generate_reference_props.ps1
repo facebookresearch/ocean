ac96c33430240cf17eb75751d10ea273de871935
Jan Herling <janherling@meta.com>
2026-01-21 16:49:50 -0800

    Added copyright header to scripts
    Summary: $title
    
    Reviewed By: enpe
    
    Differential Revision: D91174034
    
    fbshipit-source-id: 31b6b827c3a209278d6b2507b83d7eac4f129d03

--- build/visual_studio/generate_reference_props.ps1
+++ build/visual_studio/generate_reference_props.ps1
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 <#
 .SYNOPSIS
     Generates *_reference.props files from *_common.props and application *_build.props files for Ocean Visual Studio projects.
--- build/visual_studio/generate_reference_props.py
+++ build/visual_studio/generate_reference_props.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 """
 Generates *_reference.props files from *_common.props and application *_build.props
 files for Ocean Visual Studio projects.
