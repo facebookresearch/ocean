b7880fbe1d1d639ad8bba792c253574bc850bac5
Jan Herling <janherling@meta.com>
2026-02-17 17:54:23 -0800

    Added Android VS project for Devices Android JNI
    Summary: Added Visual Studio Android project files for the Devices Android JNI library pair (native C++ and Java). This includes the vcxproj, property sheets, and Gradle build files needed for building the JNI bridge and its corresponding Java AAR library.
    
    Reviewed By: enpe
    
    Differential Revision: D93489026
    
    fbshipit-source-id: 48c5d663349cad3e566109fc9b89b640207d8530

---
+++ build/visual_studio/android/vc143/ocean/devices/android/jni/gradle/settings.gradle.kts
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rootProject.name = providers.gradleProperty("projectName").getOrElse("devicesandroidjnijava")
+
+include(":lib")
+
+project(":lib").projectDir = file("../lib")
