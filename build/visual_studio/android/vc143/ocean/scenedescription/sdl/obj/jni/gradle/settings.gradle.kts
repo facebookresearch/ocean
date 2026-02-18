4de0bf3ebf814a25923bcf2b04ae92253f867f86
Jan Herling <janherling@meta.com>
2026-02-17 17:54:23 -0800

    Added Android VS project for SceneDescription SDL OBJ JNI
    Summary: Added Visual Studio Android project files for the Ocean SceneDescription SDL OBJ JNI library, following the same pattern as the Base JNI project. This includes a native C++ project (scenedescriptionsdlobjjni) that compiles SceneDescriptionSDLOBJJni.cpp and a Java library project (scenedescriptionsdlobjjnijava) that builds SceneDescriptionSDLOBJJni.java into an AAR via Gradle, along with the corresponding property sheets and Gradle wrapper files.
    
    Reviewed By: enpe
    
    Differential Revision: D93489020
    
    fbshipit-source-id: a608350b9cf2a26c9a4d98e723069bdbf770889f

---
+++ build/visual_studio/android/vc143/ocean/scenedescription/sdl/obj/jni/gradle/settings.gradle.kts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rootProject.name =
+    providers.gradleProperty("projectName").getOrElse("scenedescriptionsdlobjjnijava")
+
+include(":lib")
+
+project(":lib").projectDir = file("../lib")
